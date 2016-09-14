ea1f9d0828152660fbac55731e174c2528e13f8a
Didier <Didou09@users.noreply.github.com>
2016-09-14 10:39:08 +0200

    Create tofu/mesh/ with __init__.py
    Add already relevant __init__.py

---
+++ tofu/treat/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#! /usr/bin/python
+
+"""
+Provide data handling class and methods (storing, processing, plotting...)
+"""
+
+
+from ._core import *
+
+del _core
+
+__author__ = "Didier Vezinet"
+__all__ = ["PreData"]
+
+
