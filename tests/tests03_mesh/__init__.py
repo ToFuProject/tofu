909ca059005ca91086ff8de3b427c43fe2adde89
Didier <Didou09@users.noreply.github.com>
2016-09-14 10:18:45 +0200

    Create /tests03_mesh/ with __init__.py
    Non-empty __init__.py

---
+++ tests/tests03_mesh/__init__.py
@@ -0,0 +1,45 @@
+"""
+This module contains tests for tofu.geom in its structured version
+"""
+
+
+# Nose-specific
+from nose import with_setup # optional
+
+
+
+
+
+
+#######################################################
+#
+#     Setup and Teardown
+#
+#######################################################
+
+def setup_module(module):
+    print ("") # this is to get a newline after the dots
+    print ("")
+    print ("############################################")
+    print ("############################################")
+    print ("        test03_mesh")
+    print ("############################################")
+    print ("")
+
+
+
+def teardown_module(module):
+    #os.remove(VesTor.Id.SavePath + VesTor.Id.SaveName + '.npz')
+    #os.remove(VesLin.Id.SavePath + VesLin.Id.SaveName + '.npz')
+    #print ("teardown_module after everything in this file")
+    #print ("") # this is to get a newline
+    pass
+
+
+
+
+
+
+
+
+
