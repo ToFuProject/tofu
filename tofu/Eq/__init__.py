bd03cb4891e0e5b4054f5614a2d7d60ff1a0a627
Didier <Didou09@users.noreply.github.com>
2016-09-14 10:41:40 +0200

    Create tofu/Eq/ with __init__.py
    Add already relevant __init__.py

---
+++ tofu/Eq/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#! /usr/bin/python
+
+
+"""
+Load all core packages and modules which are all machine-independent, diagnostic-independent and code-independent
+
+
+Created on Tuesday 07 June 2016
+
+@version: 0.9
+@author: didiervezinet
+@author_email: didier.vezinet@gmail.com
+"""
+
+
+from ._core import *
+
+#del _core
+
+#__name__ = ""
+__version__ = "0.9"
+__author__ = "Didier Vezinet"
+__author_email__="didier.vezinet@gmail.com"
+__date__ = "$Monday 20 June 2016$"
+#__copyright__ = ""
+#__license__ = ""
+#__url__ = ""
+#__path__ =
+
+
+
+
+
