0c44ffb605e8082f905a0135b5bf240157cd24b4
VEZINET Didier 00/00/0000 <DV226270@spica.intra.cea.fr>
2017-10-05 18:10:53 +0200

    Try with Travis, Pypi and Anaconda Cloud 11

--- conda_recipe/conda_upload.sh
+++ conda_recipe/conda_upload.sh
@@ -7,6 +7,6 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 #export VERSION=`date +%Y.%m.%d`
-export VERSION="$(python -c 'import version; print(version.__version__)')"
+export VERSION="""$(python -c "import version; print(version.__version__.replace('-','.'))")"""
 conda build .
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-0.tar.bz2 --force
