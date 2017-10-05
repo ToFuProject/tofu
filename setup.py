e7ff1565db51c569f91b746518626ea83fc1047c
VEZINET Didier 00/00/0000 <DV226270@spica.intra.cea.fr>
2017-10-05 09:10:35 +0200

    small changes to setup.py and .travis, for debugging version fetching 13

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6'
 
         # In which language most of the code is written ?
         'Natural Language :: English',
@@ -132,6 +133,9 @@ setup(
             'nose',
             'nose-timer',
             ],
+
+    python_requires = '2.7, 3.6'
+
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
