64e31675ea9676f37c1b71c24a26d7a6b1e64a2c
VEZINET Didier 00/00/0000 <DV226270@spica.intra.cea.fr>
2017-10-06 13:25:33 +0200

    Try with Travis, Pypi and Anaconda Cloud - 2 recipes 01

---
+++ conda_recipe36/conda_upload.sh
@@ -0,0 +1,13 @@
+# Only need to change these two variables
+PKG_NAME=tofu
+USER=ToFuProject
+
+OS=linux-64
+mkdir ~/conda-bld
+conda config --set anaconda_upload no
+conda config --add channels pypi
+export CONDA_BLD_PATH=~/conda-bld
+#export VERSION=`date +%Y.%m.%d`
+export VERSION="""$(python -c "import version; print(version.__version__.replace('-','.'))")"""
+conda build .
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-0.tar.bz2 --force
