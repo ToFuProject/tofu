536b98c0da4c1971d484aa4c9917a6a599562e3c
VEZINET Didier 00/00/0000 <DV226270@spica.intra.cea.fr>
2017-10-09 09:36:15 +0200

    Try with Travis, Pypi and Anaconda Cloud - 2 recipes 16

--- conda_recipe27/conda_upload.sh
+++ conda_recipe27/conda_upload.sh
@@ -5,10 +5,11 @@ USER=Didou09
 OS=linux-64
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
-conda config --add channels pypi
+#conda config --add channels pypi
 export CONDA_BLD_PATH=~/conda-bld
 #export VERSION=`date +%Y.%m.%d`
 export VERSION=$(head -n 1 ../version.txt)
 conda build .
 echo "uploading..."
+echo $CONDA_BLD_PATH
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-0.tar.bz2 --force
