6e49f3d8dd4dc4d1e9181d35791e65df8cbda512
VEZINET Didier 00/00/0000 <DV226270@spica.intra.cea.fr>
2017-10-09 11:36:22 +0200

    Try with Travis, Pypi and Anaconda Cloud - 2 recipes 22

--- conda_recipe27/conda_upload.sh
+++ conda_recipe27/conda_upload.sh
@@ -9,7 +9,11 @@ conda update -n root conda-build
 #conda config --add channels pypi
 export CONDA_BLD_PATH=~/conda-bld
 #export VERSION=`date +%Y.%m.%d`
+echo ""
+echo "VERSION"
+echo $VERSION
 export VERSION=$(head -n 1 version.txt)
+echo $VERSION
 
 echo ""
 echo $VERSION
@@ -22,10 +26,12 @@ ls .
 
 echo "CONDA_BLD_PATH"
 echo $CONDA_BLD_PATH
-ls $CONDA_BLD_PATH
+ls $CONDA_BLD_PATH/$OS
+echo ""
+echo $PKG_NAME-$VERSION-$VADD.tar.bz2
 
 echo ""
 echo "RECIPE"
 ls $RECIPE
 
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-0.tar.bz2 --force
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-$VADD.tar.bz2 --force
