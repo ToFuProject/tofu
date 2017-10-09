b6a99c72bbafd6e8a3d0cc4f8efc05f0637dc671
VEZINET Didier 00/00/0000 <DV226270@spica.intra.cea.fr>
2017-10-09 12:06:00 +0200

    Try with Travis, Pypi and Anaconda Cloud - 2 recipes 24

--- conda_recipe27/conda_upload.sh
+++ conda_recipe27/conda_upload.sh
@@ -1,6 +1,6 @@
 # Only need to change these two variables
 PKG_NAME=tofu
-USER=Didou09
+USER=ToFuProject
 
 OS=linux-64
 mkdir ~/conda-bld
@@ -9,29 +9,7 @@ conda update -n root conda-build
 #conda config --add channels pypi
 export CONDA_BLD_PATH=~/conda-bld
 #export VERSION=`date +%Y.%m.%d`
-echo ""
-echo "VERSION"
-echo $VERSION
-export VERSION=$(head -n 1 version.txt)
-echo $VERSION
+#export VERSION=$(head -n 1 version.txt)
 
-echo ""
-echo $VERSION
-echo $RECIPE
 conda build $RECIPE
-
-echo "uploading..."
-pwd .
-ls .
-
-echo "CONDA_BLD_PATH"
-echo $CONDA_BLD_PATH
-ls $CONDA_BLD_PATH/$OS
-echo ""
-echo $PKG_NAME-$VERSION-$VADD.tar.bz2
-
-echo ""
-echo "RECIPE"
-ls $RECIPE
-
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION-$VADD.tar.bz2 --force
