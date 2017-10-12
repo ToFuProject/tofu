3131195a1e51141b42a28078156d322f9f0ae334
VEZINET Didier 00/00/0000 <DV226270@spica.intra.cea.fr>
2017-10-12 15:49:44 +0200

    Try with Travis, Pypi and Anaconda Cloud 39

--- conda_recipe27/build.sh
+++ conda_recipe27/build.sh
@@ -1,4 +1,3 @@
 # Usefed for nose-timer and Polygon
-conda --add channels conda-forge
 pip install -i https://pypi.anaconda.org/pypi/simple $PCK
 $PYTHON setup.py install     # Python command to install the script.
