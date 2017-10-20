e10b06cec696dde299f012c46c6cd4721231e59d
VEZINET Didier 00/00/0000 <DV226270@spica.intra.cea.fr>
2017-10-20 09:59:35 +0200

    Try with Travis, Pypi and Anaconda Cloud 100

--- setup.py
+++ setup.py
@@ -123,7 +123,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages = find_packages(exclude=['doc', '_Old', '_Old_doc','plugins','tests10_plugins']),
+    packages = find_packages(exclude=['doc', '_Old', '_Old_doc','tofu.plugins','tofu.tests.tests10_plugins']),
     #packages = ['tofu','tofu.geom'],
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
@@ -165,7 +165,7 @@ setup(
     #    # And include any *.csv files found in the 'ITER' package, too:
     #    'ITER': ['*.csv'],
     #},
-    package_data={'tests01_Geom':['test_Ves.txt']},
+    package_data={'tests.tests01_geom':['test_Ves.txt']},
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
