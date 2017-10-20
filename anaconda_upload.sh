75b60c0d13578835888d52b9c2917bb829a6a99d
VEZINET Didier 00/00/0000 <DV226270@spica.intra.cea.fr>
2017-10-20 17:34:55 +0200

    Simplifying/speeding .travis.yml and conda deployment - Try 03

---
+++ anaconda_upload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+echo "Converting conda package..."
+conda convert --platform all $PKG_DIR/tofu-*.tar.bz2 --output-dir $PKG_DIR
+
+echo "Deploying to anaconda.org..."
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $PKG_DIR/tofu-*.tar.bz2
+echo "Successfully uploaded !"
+exit 0
--- conda_recipe/conda_upload.sh
+++ conda_recipe/conda_upload.sh
@@ -3,12 +3,12 @@ PKG_NAME=tofu
 USER=ToFuProject
 OS=linux-64
 
-mkdir ~/conda-bld
-conda config --set anaconda_upload no
-conda update -n root conda-build
-conda config --append channels conda-forge
-conda config --append channels tofuproject
-export CONDA_BLD_PATH=~/conda-bld
+#mkdir ~/conda-bld
+#conda config --set anaconda_upload no
+#conda update -n root conda-build
+#conda config --append channels conda-forge
+#conda config --append channels tofuproject
+#export CONDA_BLD_PATH=~/conda-bld
 #export VERSION=`date +%Y.%m.%d`
 #export VERSION=$(head -n 1 version.txt)
 
