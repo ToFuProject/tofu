ffcfc166ec3049730dd3dd92c342579929094d2a
VEZINET Didier 00/00/0000 <DV226270@spica.intra.cea.fr>
2017-10-23 09:06:28 +0200

    Simplifying/speeding .travis.yml and conda deployment - Try 07

--- anaconda_upload.sh
+++ anaconda_upload.sh
@@ -5,6 +5,10 @@ set -e
 #conda convert --platform all $PKG_DIR/tofu-*.tar.bz2 --output-dir $PKG_DIR
 
 echo "Deploying to anaconda.org..."
+echo $CONDA_UPLOAD_TOKEN
+echo $USER
+echo $PKG_DIR
+echo $(ls $PKG_DIR/tofu-*.tar.bz2)
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $PKG_DIR/tofu-*.tar.bz2
 echo "Successfully uploaded !"
 exit 0
