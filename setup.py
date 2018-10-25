81f23862e47fbfcab66d2a5385e4ffbf1cbbdca1
VEZINET Didier <didier.vezinet@cea.fr>
2018-10-25 08:56:16 +0200

    Try to fix pandas dependency issue (added in dependencies, and test of tf.geom.Config.get_summary() now optional)

--- setup.py
+++ setup.py
@@ -145,6 +145,7 @@ setup(
             'matplotlib',
             poly,
             'cython',
+            'pandas',
             ],
 
     python_requires = '>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
