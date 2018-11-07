2ea74151268e6ca54db67acf660a8cd7d2f1842d
Laura Mendoza <laura.mendoza@inria.fr>
2018-11-07 09:00:12 +0100

    [Notes] readded Notes_Upgrades in the gitignore, added benchmarking and LOS intersect cone files

--- tofu/tests/tests01_geom/test00_Laura/Objects/test_LOS_west_geom.py
+++ tofu/tests/tests01_geom/test00_Laura/Objects/test_LOS_west_geom.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 from tofu_LauraBenchmarck_load_config import *
+plt.ion()
 import tofu.geom._GG as _GG
-
+import time
 
 
 def test_LOS_west_Aconfig(config) :
@@ -91,13 +92,69 @@ def test_LOS_west_Aconfig(config) :
                                         Lim=ves.Lim,
                                         VType=ves.Type)
 
+def test_LOS_west_Aconfig_short(config, cams, plot=False):
+    dconf = load_config(config, plot=plot)
+    if plot:
+        plt.show(block=True)
+    ves = dconf["Ves"]
+    times = []
+    for vcam in cams:
+        D, u = get_Du(vcam)
+        u = u/np.sqrt(np.sum(u**2, axis=0))
+        start = time.time()
+        out = _GG.LOS_Calc_PInOut_VesStruct(D, u,
+                                            ves.Poly,
+                                            ves.geom['VIn'],
+                                            Lim=ves.Lim,
+                                            VType=ves.Type)
+        elapsed = time.time() - start
+        times.append(elapsed)
+    return times
+
+
+def test_LOS_west_Bconfig(config, cams, plot=False):
+    dconf = load_config(config, plot=plot)
+    if plot:
+        plt.show(block=True)
+    ves = dconf["Ves"]
+    struct = list(dconf['Struct'].values())
+
+    for vcam in cams:
+        D, u = get_Du(vcam)
+        u = u/np.sqrt(np.sum(u**2, axis=0))
+        lSPoly = [ss.Poly for ss in struct]
+        lSLim = [ss.Lim for ss in struct]
+        lSVIn = [ss.geom['VIn'] for ss in struct]
+        start = time.time()
+        out = _GG.LOS_Calc_PInOut_VesStruct(D, u,
+                                            ves.Poly,
+                                            ves.geom['VIn'],
+                                            Lim=ves.Lim,
+                                            LSPoly = lSPoly,
+                                            LSLim=lSLim,
+                                            LSVIn=lSVIn,
+                                            VType=ves.Type)
+        elapsed = time.time() - start
+        times.append(elapsed)
+    return times
+
 if __name__ == "__main__":
+    Cams = ["V1", "V10", "V100", "V1000", "V10000",
+            "V100000", "V1000000"]
     Aconfigs = ["A1", "A2", "A3"]
+    Bconfigs = ["B1", "B2", "B3"]
     for icon in Aconfigs :
         print("*..................................*")
         print("*      Testing the "+icon+" config       *")
         print("*..................................*")
-        test_LOS_west_Aconfig(icon)
+        times = test_LOS_west_Aconfig_short(icon, Cams)
+        print(times)
 
+    for icon in Bconfigs :
+        print("*..................................*")
+        print("*      Testing the "+icon+" config       *")
+        print("*..................................*")
+        times = test_LOS_west_Bconfig(icon, Cams)
+        print(times)
 
 
