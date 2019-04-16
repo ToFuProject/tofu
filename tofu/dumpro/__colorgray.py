82baab5fe19ac876130a006c662863ec57785da2
KHANDELWAL Arpan 258850 <napraarpan@gmail.com>
2019-04-16 11:08:16 +0200

    made corrections for comments in grayscale conversion

--- tofu/dumpro/__colorgray.py
+++ tofu/dumpro/__colorgray.py
@@ -3,10 +3,13 @@
 Created on Fri Mar  8 08:48:58 2019
 
 @author: Arpan Khandelwal
+email: napraarpan@gmail.com
 
-input: video_file (The original video of the shot capturedby the camera)
-output: gray(The gray scale version of the shot on which a threshold function
-             will be applied in the next step)
+Input the video file on which the Dumpro wants the operation to be performed
+and the output is the grayscale video file that will be used for futher 
+preprocessing 
+
+You need to have Opencv 3 or greater installed, for using this subroutine
 """
 
 import numpy as np
@@ -17,49 +20,36 @@ def ConvertGray(video_file):
     
     ==========================================================================
     Parameters
-    ==========================================================================
-    input:
-    --------------------------------------------------------------------------
-     video file(mp4,avi)
-    --------------------------------------------------------------------------
-    output:
-     video file name which is in .avi format
-     
-     
-     Didier: Parameters means input in docstr, so you just need to write:
-     Parameters
-     ----------
-     video_file:   parameter type
-         parameter description
-     
-     Return
-     ------
-     file:         output type
-         output description
-     
+    -----------------------
+    video_file:      supported formats - mp4,avi
+     input video passed in as argument
+    
+    Return
+    -----------------------
+    File:            Grayscale.avi
+     video file which is in .avi format    
      
     """
 
     # Didier: all comments indentation should be aligned with code identation
     # I correct some of them below for example
     
-# reading the input file   
+    # reading the input file   
     cap = cv2.VideoCapture(video_file)
     #read the first frame    
     ret,frame = cap.read()
 
-#describing the four character code fourcc  
+    #describing the four character code fourcc  
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     #getting frame height and width
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
     
-#videowriter writes the new video with the frame height and width and fps   
-#videowriter(videoname, format, fps, dimensions_of_frame,)
+    #videowriter writes the new video with the frame height and width and fps   
+    #videowriter(videoname, format, fps, dimensions_of_frame,)
     out = cv2.VideoWriter('Grayscale.avi',fourcc, 25 ,(frame_width,frame_height),0)    
-    
-#loops over the entire video frame by frame and convert each to grayscale
-#then writting it to output file     
+    #loops over the entire video frame by frame and convert each to grayscale
+    #then writting it to output file     
     while(cap.isOpened()):
         ret, frame = cap.read()
         #to check whether cap read the file successfully         
@@ -67,9 +57,9 @@ def ConvertGray(video_file):
         #conversion from RGB TO GRAY frame by frame        
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame = gray
-#writing the gray frames to out        
+        #writing the gray frames to out        
         out.write(frame)
- #closing everything       
+    #closing everything       
     cap.release()
     out.release()
     cv2.destroyAllWindows()
