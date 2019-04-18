6c4627683ccfef70771c55edf2bf3779566f5c48
Arpan <napraarpan@gmail.com>
2019-04-18 22:44:34 +0200

    added subroutines to computation module

--- tofu/dumpro/_comp.py
+++ tofu/dumpro/_comp.py
@@ -1,1 +1,12 @@
 # computation routines (functions called from _core.py)
+
+#Grayscale conversion routine
+import __colorgray
+#Background computation routine
+import __background_removal
+#video splitting Routine
+import __video_to_img
+#Image to numpy array conversion routine
+import __image_2_array
+
+
