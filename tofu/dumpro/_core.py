366d78811c0dffba7e1a1917e48ac78eb6c5ddd9
KHANDELWAL Arpan 258850 <napraarpan@gmail.com>
2019-04-19 12:52:41 +0200

    modified definitions of grascale method

--- tofu/dumpro/_core.py
+++ tofu/dumpro/_core.py
@@ -13,11 +13,17 @@ import scipy as scp
 import cv2
 import matplotlib.pyplot as plt
 import datetime
+
+#more special packages
+try:
+    import cv2
+except ImportError:
+    print("Could not find opencv package. Try pip intall opencv-contrib-python")
 # tofu-specific
 #import tf.__version__ as __version__
 
 # dumpro-specific
-import _comp
+import computation as _comp
 # !!!! never use 'from ... import *'
 # Always remain explicit !!! (e.g.: np.cos() and not cos())
 
@@ -35,7 +41,7 @@ class Video(object):
    
     """
     
-    def __init__(self, filename):
+    def __init__(self, filename, time_window):
         
         # join is for joining together several path parts
         # os.path.abspath(path) returns the absolute path of your path
@@ -43,21 +49,22 @@ class Video(object):
         # you can split the char str into path + file
         # path, filename = os.path.split(filename)
         
-        path = os.path.abspath(path)
-        try:
-            if not os.path.exists(path): # os.path.isfile
-                msg = "The provided path does not exist:\n"
-                msg += "\t-path: %s"%path
-                msg += "\t=> Please provide the correct path and try again" 
-                raise Exception(msg)
-            else:
-                self.cap = cv2.VideoCapture(path)
+#        path = os.path.abspath(path)
+#        try:
+#            if not os.path.isfile(path): # os.path.isfile(path)
+#                msg = "The provided path does not exist:\n"
+#                msg += "\t-path: %s"%path
+#                msg += "\t=> Please provide the correct path and try again" 
+#                raise Exception(msg)
+#                
+            self.cap = cv2.VideoCapture(filename)
                     
-                self.__frame_width = int(self.cap.get(3))
-                self.__frame_height = int(self.cap.get(4))
-                self.__video_time = self.cap.get(cv2.CAP_PROP_POS_MSEC)
-                self.__N_frames = self.cap.get(cv2.CAP_PROP_POS_FRAMES)
-                self.__fps = self.cap.get(cv2.CAP_PROP_FPS)
+            self.__frame_width = int(self.cap.get(3))
+            self.__frame_height = int(self.cap.get(4))
+            self.__video_time = self.cap.get(cv2.CAP_PROP_POS_MSEC)
+            self.__N_frames = self.cap.get(cv2.CAP_PROP_POS_FRAMES)
+            self.__fps = self.cap.get(cv2.CAP_PROP_FPS)
+        
         
     def resolution(self):
         
@@ -82,12 +89,16 @@ class Video(object):
 #        
 #        
 #    
-    def dumpro(self):
+    def grayscale(self,path,output_name,output_type):
         """ Create a new instance with the grayscale-converted video """
         
-        gray = __colorgray.ConvertGray(video_file)
+        gray = _comp.__colorgray.ConvertGray(self,path,output_name,output_type)
             
         return self.__class__(gray)
-
-
+    
+    def removebackground(self):
+        
+        foreground = _comp.__background_removal.Background_Removal(self)
+        
+        return self.__class__(foreground)
     
