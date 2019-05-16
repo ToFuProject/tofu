9bb701af8991237e0086bfd2c0dad5b383956f6e
Arpan <napraarpan@gmail.com>
2019-05-16 05:52:27 +0200

    updated defination with more functions

--- tofu/dumpro/computation/__init__.py
+++ tofu/dumpro/computation/__init__.py
@@ -2,4 +2,4 @@ from . import colorgray
 from . import background_removal
 from . import video_to_img 
 #Image to numpy array conversion routine
-from . import image_2_array
+from . import video_to_array
