1cc3eee5d79e50111ec8d98d5f552642bc7111db
Arpan <napraarpan@gmail.com>
2019-05-24 23:54:43 +0200

    updated arguments, docstring and comments

--- tofu/dumpro/computation/video_to_img.py
+++ tofu/dumpro/computation/video_to_img.py
@@ -2,54 +2,92 @@
 """
 Created on Wed Apr  3 10:48:03 2019
 
-@author: napra
+@author: Arpan Khandelwal
+email: napraarpan@gail.com
 
 input: video_file (The Grayscale converted video)
 output: all the frames in video are converted to images
 """
 
 
-#standard
+#nuilt in
 import os
-import warnings
 
-#built-ins
+#standard
 import numpy as np
+
+#special
 try:
     import cv2
 except ImportError:
-    print("Cannot find opencv package. Try pip intall opencv-contrib-python")
+    print("Could not find opencv package. Try pip intall opencv-contrib-python")
 
 
-# Playing video from file:
-def video2imgconvertor(video_file, path = './data'):
+def video2img(video_file, path = None, image_name = None, image_type = None):
     """Breaks up an input video file into it's constituent frames and 
     saves them as jpg image
     
     Parameters
     -----------------------
-    video_file:      mp4/avi
+    video_file:      mp4,avi,mpg
      input video passed in as argument
     path:            string
-     Path where the user wants to save the images
-    Return
+     Path where the user wants to save the images. By it will try to make a 
+     folder in the same directory as the video, by the name data. If it fails 
+     in creating the directory due t permissions, then it will give out an 
+     error.
+    image_name:      string
+     Name of the image file. The frame number will be appended to the name when
+     writting the image file. By default it is 'frame'
+    image_type:      string
+     format of the image. By default it is .jpg
+     Return
     -----------------------
     File:            String
      Path where the images are stored    
      
     """
+    #splitting the video file into drive and path + file
+    drive, path_file = os.path.splitdrive(video_file)
+    #splitting the path + file 
+    path_of_file, file = os.path.split(path_file)
+    # splitting the file to get the name and the extension
+    file = file.split('.')
+    
+    #checking for the path of the file
+    folder = 'data'
+    if path is None:
+        #the directory path is defined.  
+        #The last argument is for adding a trailing slash
+        path = os.path.join(drive,path_of_file,'')
+        #defining the folder inside whch the images will be stored
+        path += folder
+        path = os.path.join(path,'')
+        #checking whether path already exists or not
+        #if not, creates the path
+        if not os.path.exists(path):
+            os.mkdir(path)
+        
+    #checking for the name of the output file
+    if image_name is None:
+        image_name = 'frame'
+    #checking for the putput format of the video
+    if image_type is None:
+        image_type = '.jpg'
     
     print("Converting Video to Images ...... Please Wait")
     
     #trying to open the video file
     try:
-        #Reading the video file
-        if os.path.isfile(video_file):
-            cap = cv2.VideoCapture(video_file)
-    #incase of error in file name or path raising exception    
-    except IOError:
-        print("Path or file name incorrect or file does not exist")
-          
+        if not os.path.isfile(video_file):
+            raise Exception
+        cap = cv2.VideoCapture(video_file)
+        
+    except Exception:
+        msg = 'the path or filename is incorrect.'
+        msg += 'PLease verify the path or file name and try again'
+        raise Exception(msg)
+    
     #Creating Directory
     try:
         if not os.path.exists(path):
@@ -72,14 +110,17 @@ def video2imgconvertor(video_file, path = './data'):
         #To break out of loop when conversion is done
         #ret reads false after we have exhausted through our frames 
         if not ret: break
-        # Saves image of the current frame in jpg file
+    
+        # Saves image of the current frame in user defined format 
+        #or by default jpg file
         #frame number starts from 0
-        name = path + 'frame'+ str(currentFrame) + '.jpg'
+        name = path + image_name + str(currentFrame) + image_type
+        print('Converting frame :', currentFrame)
         cv2.imwrite(name, frame)
         
         # To stop duplicate images
         currentFrame += 1
-        
+    print('Total number of frames converted :', currentFrame)    
     # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
