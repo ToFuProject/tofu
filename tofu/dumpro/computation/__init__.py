07b346c06017c8e07d291973f55009c9758491a1
Arpan <napraarpan@gmail.com>
2019-05-26 13:02:13 +0200

    updated computation package

--- tofu/dumpro/computation/__init__.py
+++ tofu/dumpro/computation/__init__.py
@@ -1,5 +1,12 @@
+#A master module containing all the subroutines required for computation
+#Most subroutines require Opencv 3 or greater version
+
+
+
 from . import colorgray
 from . import background_removal
 from . import video_to_img 
 #Image to numpy array conversion routine
 from . import video_to_array
+from . import playvideo
+from . import edge_detection
