418aafdf02aae24923fa68376f62a6c46ff4b6b6
Arpan <napraarpan@gmail.com>
2019-05-28 10:12:20 +0200

    updated docstring of methods

--- tofu/dumpro/_core.py
+++ tofu/dumpro/_core.py
@@ -13,6 +13,7 @@ email: napraarpan@gmail.com
 # Built-ins
 import os
 import warnings
+import inspect
 
 # Standard
 import numpy as np
@@ -60,7 +61,7 @@ class Video(object):
     
     """
 
-    def __init__(self, filename):
+    def __init__(self, filename, verb=True):
 
         #checking if the file provided exists or not
         if not os.path.isfile(filename): 
@@ -70,15 +71,31 @@ class Video(object):
             raise Exception(msg)
         #reading the video file
         self.cap = cv2.VideoCapture(filename)
+        if verb:
+            msg = "The following file was successfully loaded:\n"
+            msg += "    %s"%filename
+            msg += "    {}".format(filename)
+            print(msg)
+        self.__filename = filename
         #getting the meta data of the video
         self.__frame_width = int(self.cap.get(3))
         self.__frame_height = int(self.cap.get(4))
         self.__video_time = self.cap.get(cv2.CAP_PROP_POS_MSEC)
         self.__N_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
         self.__fps = self.cap.get(cv2.CAP_PROP_FPS)
+        self.__fourcc = int(self.cap.get(cv2.CAP_PROP_FOURCC))
         #calculating the duration of the video        
         self.__duration = self.__N_frames/self.__fps
-#        self.__reshape = {'time': (t1,t2), 'size':((x1,x2),(y1,y2))}
+        self.meta_data = {'fps' : self.__fps, 
+                          'fourcc' : self.__fourcc, 
+                          'N_frames' : self.__N_frames,
+                          'frame_width' : self.__frame_width,
+                          'frame_height' : self.__frame_height}
+        
+    @property
+    def filename(self):
+        return self.__filename
+    
     
     #defining a getter for the video resolution
     def resolution(self):
@@ -89,120 +106,80 @@ class Video(object):
         height = self.__frame_height
         return (width,height)
     
-     #defining  a getter for the video duration
-    def video_time(self):
-        """Returns the  """
-        return self.__video_time
-    
     #defining a getter for the total number of frames in the video
-    def frame_count(self):
+    @property
+    def N_frames(self):
         """ Returns the total number of frames in the video"""
-        
         return self.__N_frames
     
     #defining a getter for the frames per second
+    @property
     def fps(self):
         """Returns the playspeed of the video in frames per second"""
-        
         return self.__fps
     
-    #defining a getter for the video duration
-    def duration(self):
-        
-        return self.__duration
+    #defining a gettter for the four character code of the video
+    @property
+    def fourcc(self):
+        """ Returns the four character code of the video"""
+        return self.__fourcc
     
-#    def slice_video(self,(t1,t2),((x1,x2),(y1,y2)):
-#        
-#        self.__reshape = {'time': (t1,t2), 'size' :((x1,x2),(y1,y2))}
+    #defining a method for grayscale conversion
+    def grayscale(self, meta_data = None, path = None,
+                  output_name = None, output_type = None):
 
-    def grayscale(self,path = None,output_name = None,output_type = None):
-        """Converts input video file to grayscale, denoises it and saves it as 
-        Grayscale.avi
-    
-        The denoising process is very processor intensive and takes time, 
-        but it give very good results. 
-        
-        Parameters
-        -----------------------
-        video_file:       mp4,avi
-         input video along with its path passed in as argument
-        path:             string
-         Path where the user wants to save the video. By default it take the path 
-         from where the raw video file was loaded
-        output_name:      String
-         Name of the Grayscale converted video. By default it appends to the 
-         name of the original file '_grayscale'
-        output_type:      String
-         Format of output defined by user. By default it uses the format of the 
-         input video
-        
-        Return
-        -----------------------
-        pfe:              String
-         Path along with the name and type of video    
-        meta_data:        dictionary
-         A dictionary containing the meta data of the video.
-    """
-        
         #gray will contain the video path and meta_data will contain the 
         #size of frames, total number of frames and the fps of the video
-        gray, meta_data = _comp.colorgray.convertgray(self, path,output_name, output_type)
+        gray, meta_data = _comp.colorgray.convertgray(self.__filename,self.meta_data, path,
+                                                      output_name, output_type)
         #returning the grayscale converted video as a new instance 
         return self.__class__(gray)
 
-    def removebackground(self,path = None,output_name = None,output_type = None):
-        """ Removes the background of the video and returns the foreground
-        as a new instance"""
-        
+    def removebackground(self, meta_data = None, path = None,output_name = None, output_type = None):
+               
         #applying the background removal operation
-        foreground, meta_data = _comp.background_removal.remove_background(self,path,output_name,output_type)
+        if meta_data == None:
+            meta_data = self.meta_data
+        foreground, meta_data = _comp.background_removal.remove_background(self.__filename, meta_data, path, output_name, output_type)
         return self.__class__(foreground)
     
     def applybinary(self,path = None,output_name = None,output_type = None):
-        """ Applies binary threshold to the video and then creates a new instance
-        with the binary video"""
         
         #applying the method of binary conversion
-        binary,meta_data = _comp.binarythreshold.binary_threshold(self,path,output_name,output_type)
+        out = _comp.binarythreshold.binary_threshold(self.__filename,path,
+                                                     output_name,output_type)
         #returning the binary converted video as a new instance
-        return self.__class__(binary)
+        return self.__class__(out[0])
     
-    def play(self):
-        """ Displays the video"""
-        #playing the video
-        _comp.playvideo.play_video(self)
         
     def detectedge(self, path = None, output_name = None, output_type = None):
-        """ detects edges in the video and returns the video as a new instance
-        """
         
         #applying the edge detection method
-        edge, meta_data = _comp.edge_detection.detect_edge(self, path,output_name,output_type)
+        edge, meta_data = _comp.edge_detection.detect_edge(self.__filename, path,output_name,output_type)
         #returns the edge detected video as a new instance
         return self.__class__(edge)
     
     def convert2image(self, path = None , image_name = None, image_type = None):
-        """Breaks the video up into its frames and saves them"""
         
         #applying the video to image conversion method
-        directory = _comp.video_to_img.video2img(self, path, image_name, image_type)
+        directory = _comp.video_to_img.video2img(self.__filename, path, image_name, image_type)
         #returning the directory in which the video is stored
         return directory
         
     def convert2pixel(self):
-        """Converts the video into numpy array and returns the array"""
         
         #applying the video to array conversion method
-        pixel, meta_data = _comp.video_to_array.video_to_pixel(self)
+        pixel, meta_data = _comp.video_to_array.video_to_pixel(self.__filename)
         
         return pixel, meta_data
+    
     
     def dumpro(self, path = None, output_name = None, output_type = None):
         
         print('Performing Preprocessing on the Video')
         print('Performing Grayscale conversion and Noise Removal')
         
-        gray, meta_data = _comp.colorgray.convertgray(self, path, output_name, output_type)
+        gray, meta_data = _comp.colorgray.convertgray(self.__filename, path, output_name, output_type)
         print('grayscale conversion complete: video stored at: '+str(gray))
         
         print('Performing background removal')
@@ -210,3 +187,14 @@ class Video(object):
         print('background removal complete: video stored at: '+str(foreground))
         
         
+#sig = inspect.signature(_comp.colorgray.convertgray)
+#lp = [p for p in sig.parameters.values() if p.name != 'video_file']
+#Video.grayscale.__signature__ = sig.replace(parameters = lp)
+        
+#Applying the docstring of functions to class methods
+Video.grayscale.__doc__ = _comp.colorgray.convertgray.__doc__
+Video.removebackground.__doc__ = _comp.background_removal.remove_background.__doc__
+Video.applybinary.__doc__ = _comp.binarythreshold.binary_threshold.__doc__
+Video.detectedge.__doc__ = _comp.edge_detection.detect_edge.__doc__
+Video.convert2pixel.__doc__ = _comp.video_to_array.video_to_pixel.__doc__
+Video.convert2image.__doc__ = _comp.video_to_img.video2img.__doc__
