9c4d8e8338ecbd9e190625d0b27c28253a0cae8b
VEZINET Didier <didier.vezinet@cea.fr>
2019-06-07 13:16:28 +0200

    imas2tofu : added MultiIDSLOader and loading routines to __init__ from _utils

--- tofu/imas2tofu/__init__.py
+++ tofu/imas2tofu/__init__.py
@@ -16,13 +16,19 @@ except Exception:
     raise Exception(msg)
 
 try:
-    from tofu.imas2tofu._configloader import load_Config
-    from tofu.imas2tofu._plasma2Dloader import load_Plasma2D
-    from tofu.imas2tofu._diagloader import load_Diag
+    from tofu.imas2tofu._utils import MultiIDSLoader
+    from tofu.imas2tofu._utils import load_Config, load_Plasma2D
+
+    # from tofu.imas2tofu._configloader import load_Config
+    # from tofu.imas2tofu._plasma2Dloader import load_Plasma2D
+    # from tofu.imas2tofu._diagloader import load_Diag
 except Exception:
-    from ._configloader import load_Config
-    from ._plasma2Dloader import load_Plasma2D
-    from ._diagloader import load_Diag
+    from ._utils import MultiIDSLoader
+    from ._utils import load_Config, load_Plasma2D
+
+    # from ._configloader import load_Config
+    # from ._plasma2Dloader import load_Plasma2D
+    # from ._diagloader import load_Diag
 
 
-__all__ = ['load_Config','load_Plasma2D','load_Diag']
+__all__ = ['MultiIDSLoader', 'load_Config','load_Plasma2D']#'load_Diag']
