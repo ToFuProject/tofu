495b1f7f1be7dbe27a0d916efea3a374591f3ff5
maskerinus <napraarpan@gmail.com>
2019-06-18 09:54:42 +0200

    updated docstring and comments

--- tofu/dumpro/image_comp/average_area.py
+++ tofu/dumpro/image_comp/average_area.py
@@ -27,16 +27,25 @@ def get_area(clus_area, t_clusters):
     avg_area           float
      The average area of the clusters
     """
+    #total area 
     area = 0
+    #converting list to array
     clus_area = np.array(clus_area)
+    #looping through array
     for c in clus_area:
+        #if c is empty list, go to next frame
         if c != []:
+            #converting list to array
             c = np.array(c)
+            #adding up all the elements 
             area += c.sum()
     
+    #converting total cluster list to array
     t_clusters = np.array(t_clusters)
-    
+    #total number of cluster present in shot
     n_clus = t_clusters.sum()
     print(n_clus)
+    #calculating average area
     avg_area = area/n_clus
+    
     return area, avg_area
--- tofu/dumpro/image_comp/average_distance.py
+++ tofu/dumpro/image_comp/average_distance.py
@@ -29,37 +29,45 @@ def get_distance(clus_center, clus_area, t_clusters):
      The average distance between two clusters in adjascent frames, taking into
      account the standard deviation
     """
+    #list of all cluster distance
     clus_dist = []
-    for i in range(len(clus_center)):
+    #looping through all the cluster centers
+    for t in range(len(clus_center)):
         print(i)
         #checking for frames without any clusters
-        if clus_center[i] != []:
-            for c in clus_center[i]:
+        if clus_center[t] != []:
+            #getting cluster in frame t
+            for c in clus_center[t]:
                 center1 = c
                 print(center1)
-                for c in clus_center[i+1]:
-                    if clus_center[i+1] != []:
+                #getting cluster in frame t+1
+                for c in clus_center[t+1]:
+                    if clus_center[t+1] != []:
                         center2 = c
                         print(center2)
+                        #calulating distance
                         x2=((center2[0]-center1[0])**2)
                         y2=((center2[1]-center1[1])**2)
-         
                         dist = (x2 + y2)**0.5
                         print(dist)
+                        #adding to cluster distance list
                         clus_dist.append(dist)
                         a = input('press c :')
                         if a == 'c':
                             continue
-                        
+    #converting clus_dist to array                    
     clus_dist = np.array(clus_dist)
+    #adding up all distances
     tot_dist = clus_dist.sum()
     print(tot_dist)
+    #calculating average distance
     avg_dist = clus_dist.mean()
+    #getting the maximum distance between two clusters
     maxi = clus_dist.max()
-    print(maxi)
+    #getting the minimum distance between two clusters
     mini = clus_dist.min()
-    print(mini)
-    std_dev = maxi - mini
+    #calculating standard deviation
+    std_dev = clus_dist.std()
     print(std_dev)
     print(len(clus_dist))
     avg_dist_big = avg_dist + 2*std_dev
