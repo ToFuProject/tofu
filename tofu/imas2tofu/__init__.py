b8af8b11f08fbc420e7e7367ad1f5475ff0d906c
VEZINET Didier <didier.vezinet@cea.fr>
2019-06-18 16:20:49 +0200

    Making sure tf.imas2tofu.__init__ works properly

--- tofu/imas2tofu/__init__.py
+++ tofu/imas2tofu/__init__.py
@@ -9,13 +9,15 @@ import warnings
 try:
     import imas
     try:
-        from tofu.imas2tofu._configloader import load_Config
-        from tofu.imas2tofu._plasma2Dloader import load_Plasma2D
-        from tofu.imas2tofu._diagloader import load_Diag
+        from tofu.imas2tofu._core import MultiIDSLoader
+        from tofu.imas2tofu._core import load_Config
+        from tofu.imas2tofu._core import load_Plasma2D
+        from tofu.imas2tofu._core import load_Diag
     except Exception:
-        from ._configloader import load_Config
-        from ._plasma2Dloader import load_Plasma2D
-        from ._diagloader import load_Diag
+        from ._core import MultiIDSLoader
+        from ._core import load_Config
+        from ._core import load_Plasma2D
+        from ._core import load_Diag
 except Exception:
     msg = "IMAS python API issue\n"
     msg += "imas could not be imported into tofu ('import imas' failed):\n"
