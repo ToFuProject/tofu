Debugged Jack's problem with wrongly calculated pinhole cameras (3 issues: missing index in u leading wrong pinhole shape, sign issue in k computation, tolerance issue in test)

tofu/data/_plot.py
@@ -479,8 +479,21 @@ def _DataCam12D_plot(lData, key=None, nchMax=_nchMax, ntMax=_ntMax,
         if lData[ii].dextra is not None:
             for k in set(lkEqmap).intersection(lData[ii].dextra.keys()):
                 idteq = id(lData[ii].dextra[k]['t'])
+
                 if idteq not in dteq[ii].keys():
+                    # test if any existing t matches values
+                    lidalready = [[k1 for k1,v1 in v0.items()
+                                   if (v1.size == lData[ii].dextra[k]['t'].size
+                                       and np.allclose(v1, lData[ii].dextra[k]['t']))]
+                                  for v0 in dteq.values()]
+                    lidalready = list(set(itt.chain.from_iterable(lidalready)))
+                    assert len(lidalready) in [0,1]
+                    if len(lidalready) == 1:
+                        idteq = lidalready[0]
+
                     dteq[ii][idteq] = lData[ii].dextra[k]['t']
+                idteq = list(dteq[ii].keys())[0]
+
                 dlextra[k][ii] = dict([(kk,v)
                                         for kk,v in lData[ii].dextra[k].items()
                                         if not kk == 't'])
@@ -493,6 +506,7 @@ def _DataCam12D_plot(lData, key=None, nchMax=_nchMax, ntMax=_ntMax,
                 msg += "    - lData[%s]: %s:\n"%(ii,lData[ii].Id.SaveName)
                 msg += "        - " + "\n        - ".join(lkEqmap)
                 warnings.warn(msg)
+
 
 
     #########
@@ -621,20 +635,24 @@ def _DataCam12D_plot(lData, key=None, nchMax=_nchMax, ntMax=_ntMax,
                              color='k', fontweight='bold',
                              fontsize=6., ha='left', va='center')
 
-    # dref
-    lref = [(lidt[ii],{'group':'time', 'val':lt[ii], 'inc':inct})
-            for ii in range(0,nDat)]
-    lref += [(lidX[ii],{'group':'channel', 'val':lX[ii], 'inc':incX,
-                        'otherid':lXother[ii], 'indother':lindtX[ii]})
-             for ii in range(0,nDat)]
-    llrr = [[(k,v) for k,v in dteq[ii].items()] for ii in range(0,nDat)]
-    llrr = itt.chain.from_iterable(llrr)
-    lref += [(kv[0], {'group':'time', 'val':kv[1], 'inc':inct}) for kv in llrr]
-    dref = dict(lref)
 
-    if nD == 2:
-        for ii in range(0,nDat):
+    # dref
+    dref = {}
+    for ii in range(0,nDat):
+        dref[lidt[ii]] = {'group':'time', 'val':lt[ii], 'inc':inct}
+        dref[lidX[ii]] = {'group':'channel', 'val':lX[ii], 'inc':incX,
+                          'otherid':lXother[ii], 'indother':lindtX[ii]}
+        if nD == 2:
             dref[lidX[ii]]['2d'] = (x1,x2)
+
+    for ii in range(0,nDat):
+        if len(list(dteq[ii])) > 0:
+            idteq, teq = list(dteq[ii].items())[0]
+            break
+    else:
+        idteq, teq = lidt[0], lt[0]
+    dref[idteq] = {'group':'time', 'val':teq, 'inc':inct}
+
 
     # ddata
     ddat = dict([(liddata[ii], {'val':ldata[ii], 'refids':[lidt[ii],lidX[ii]]})

tofu/geom/_core.py
@@ -2990,6 +2990,9 @@ class Rays(utils.ToFuObject):
 
     def _complete_dX12(self, dgeom):
 
+        import ipdb     # DB
+        ipdb.set_trace()# DB
+
         # Test if unique starting point
         if dgeom['case'] in ['A','B','C']:
             # Test if pinhole
@@ -2998,11 +3001,13 @@ class Rays(utils.ToFuObject):
                 sca2 = np.sum(dgeom['u'][:,1:]*u,axis=0)**2
                 if np.all(sca2 < 1.-1.e-9):
                     DDb = dgeom['D'][:,1:]-dgeom['D'][:,0:1]
-                    k = np.sum(DDb*(u + np.sqrt(sca2)*dgeom['u'][:,1:]),axis=0)
+                    k = np.sum(DDb*(u - np.sqrt(sca2)*dgeom['u'][:,1:]),axis=0)
                     k = k / (1.-sca2)
-                    if k[0] > 0 and np.all(k[1:]-k[0]<1.e-9):
-                        pinhole = dgeom['D'][:,0] + k[0]*u
+                    if k[0] > 0 and np.allclose(k,k[0], atol=1.e-3, rtol=1.e-6):
+                        pinhole = dgeom['D'][:,0] + k[0]*u[:,0]
                         dgeom['pinhole'] = pinhole
+
+            ipdb.set_trace()# DB
 
             # Test if all D are on a common plane or line
             va = dgeom['D']-dgeom['D'][:,0:1]

tofu/imas2tofu/_core.py
@@ -231,7 +231,7 @@ class MultiIDSLoader(object):
                            'quant':'etendue', 'units':'m2.sr'}},
 
                'spectrometer_visible':
-               {'t':{'str':'time',
+               {'t':{'str':'channel[chan].grating_spectrometer.radiance_spectral.time',
                      'quant':'t', 'units':'s'},
                 'spectra':{'str':'channel[chan].grating_spectrometer.radiance_spectral.data',
                            'quant':'radiance_spectral', 'units':'ph/s/(m2.sr)/m'},
@@ -2223,7 +2223,7 @@ class MultiIDSLoader(object):
 
     def to_Data(self, ids=None, dsig=None, mainsig=None, tlim=None,
                 indch=None, Name=None, occ=None, config=None,
-                equilibrium=True, t0=None, plot=True):
+                equilibrium=None, t0=None, plot=True):
 
         # dsig
         data, geom, dsig = self._checkformat_Data_dsig(ids, dsig,
@@ -2286,11 +2286,15 @@ class MultiIDSLoader(object):
         # data
         lk = sorted(dsig.keys())
         dins = dict.fromkeys(lk)
-        indt = self._checkformat_tlim(self.get_data(ids, sig='t')['t'],
-                                      tlim=tlim)['indt']
+        t = self.get_data(ids, sig='t', indch=indch)['t']
+        if t.ndim == 2:
+            assert np.all(np.isclose(t, t[0:1,:]))
+            t = t[0,:]
+        dins['t'] = t
+        indt = self._checkformat_tlim(t, tlim=tlim)['indt']
         out = self.get_data(ids, sig=[dsig[k] for k in lk],
                             indt=indt, indch=indch)
-        for kk in lk:
+        for kk in set(lk).difference('t'):
             if kk in ['data','X','lamb']:
                 if not isinstance(out[dsig[kk]], np.ndarray):
                     msg = "The following is supposed to be a np.ndarray:\n"
@@ -2326,6 +2330,8 @@ class MultiIDSLoader(object):
 
         # Extra
         dextra = {}
+        if equilibrium is None:
+            equilibrium = 'equilibrium' in self._dids.keys()
         if equilibrium:
             indt = self._checkformat_tlim(self.get_data('equilibrium',
                                                         sig='t')['t'],
@@ -2380,8 +2386,6 @@ class MultiIDSLoader(object):
                 dins['t'] = dins['t'] - t0
             for tt in dextra.keys():
                 dextra[tt]['t'] = dextra[tt]['t'] - t0
-
-
 
 
         import tofu.data as tfd
@@ -2875,9 +2879,6 @@ def _save_to_imas_CamLOS1D( obj, idd=None, shotfile=None,
         nchMax = obj.Id.dUSR.get('imas_nchMax', lind.max()+1)
     else:
         nchMax = lind.max()+1
-
-    import ipdb
-    ipdb.set_trace()
 
     # Fill in data
     # ------------------