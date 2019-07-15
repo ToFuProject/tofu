5c67770c76a843e5125f0de87c5d67d707869ee7
maskerinus <napraarpan@gmail.com>
2019-07-15 07:00:04 +0200

    updated description

--- tofu/dumpro/__init__.py
+++ tofu/dumpro/__init__.py
@@ -1,16 +1,18 @@
 """ DUMPRO is a pyhton library for performing dust movie processing on tokamak
- shots and then gather statistics on dust distribution
+ shots and then gather statistics on dust distribution. It is a subpackage of
+ Tofu.
 
-It consists of 3 packages.
+DUMPRO consists of 3 subpackages.
 -----------------------------
-video_comp
-image_comp
-plotting
+video_comp  : Performs dust tracking computations on videos
+image_comp  : Performs dust tracking computation on Images
+plotting    : Plots trajectories and statistics on dust distribuitions
+
+Created on February 25, 2019
 
 @author: Arpan Khandelwal
 @email: napraarpan@gmail.com
 """
-
 
 #importing core of dumpro
 from ._core import *
