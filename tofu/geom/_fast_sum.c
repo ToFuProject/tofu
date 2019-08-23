7ad63ea9f583cf6edcce0e9d75a70219a3b49960
Laura Mendoza <laura.mendoza@inria.fr>
2019-08-23 12:59:25 +0200

    [Issue142] forced add sum file

---
+++ tofu/geom/_fast_sum.c
@@ -0,0 +1,27 @@
+// C - algorithm for fast sum of a matrix (similar to np.sum(), sum row by row)
+#define MAX 8
+void sum_rows_blocks(double *orig, double *out, int n_rows, int n_cols){
+   int n_blocks=n_rows/MAX;
+   for (int b=0; b<n_blocks; b++){   //for every block
+       double res[MAX]={0.0};        //initialize to 0.0
+       for(int j=0;j<n_cols;j++){
+           for(int i=0;i<MAX;i++){   //calculate sum for MAX-rows simultaniously
+              res[i]+=orig[(b*MAX+i)*n_cols+j];
+           }
+       }
+       for(int i=0;i<MAX;i++){
+             out[b*MAX+i]=res[i];
+       }
+   }  
+   //left_overs:
+   int left=n_rows-n_blocks*MAX;
+   double res[MAX]={0.0};         //initialize to 0.0
+   for(int j=0;j<n_cols;j++){
+       for(int i=0;i<left;i++){   //calculate sum for left rows simultaniously
+              res[i]+=orig[(n_blocks*MAX)*n_cols+j];
+       }
+   }
+   for(int i=0;i<left;i++){
+         out[n_blocks*MAX+i]=res[i];
+   }
+}
