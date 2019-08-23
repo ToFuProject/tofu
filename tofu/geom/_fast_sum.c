822b94c147d24ecfc3f5939a94496faf2d71bb8f
Laura Mendoza <laura.mendoza@inria.fr>
2019-08-23 13:55:50 +0200

    [issue142] trying to fix c99 error

--- tofu/geom/_fast_sum.c
+++ tofu/geom/_fast_sum.c
@@ -2,26 +2,29 @@
 #define MAX 8
 void sum_rows_blocks(double *orig, double *out, int n_rows, int n_cols){
    int n_blocks=n_rows/MAX;
-   for (int b=0; b<n_blocks; b++){   //for every block
+   int b;
+   int j;
+   int i;
+   for (b=0; b<n_blocks; b++){   //for every block
        double res[MAX]={0.0};        //initialize to 0.0
-       for(int j=0;j<n_cols;j++){
-           for(int i=0;i<MAX;i++){   //calculate sum for MAX-rows simultaniously
+       for(j=0;j<n_cols;j++){
+           for(i=0;i<MAX;i++){   //calculate sum for MAX-rows simultaniously
               res[i]+=orig[(b*MAX+i)*n_cols+j];
            }
        }
-       for(int i=0;i<MAX;i++){
+       for(i=0;i<MAX;i++){
              out[b*MAX+i]=res[i];
        }
    }  
    //left_overs:
    int left=n_rows-n_blocks*MAX;
    double res[MAX]={0.0};         //initialize to 0.0
-   for(int j=0;j<n_cols;j++){
-       for(int i=0;i<left;i++){   //calculate sum for left rows simultaniously
+   for(j=0;j<n_cols;j++){
+       for(i=0;i<left;i++){   //calculate sum for left rows simultaniously
               res[i]+=orig[(n_blocks*MAX)*n_cols+j];
        }
    }
-   for(int i=0;i<left;i++){
+   for(i=0;i<left;i++){
          out[n_blocks*MAX+i]=res[i];
    }
 }
