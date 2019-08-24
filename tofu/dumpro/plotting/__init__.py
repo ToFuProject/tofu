a454733289a028e4b277633dcd210584e2000a24
maskerinus <napraarpan@gmail.com>
2019-08-24 19:42:40 +0200

    updated defination of plotting sub-package

--- tofu/dumpro/plotting/__init__.py
+++ tofu/dumpro/plotting/__init__.py
@@ -1,9 +1,48 @@
+"""Plotting is a sub-package for displaying all information processed and 
+gathered by DUMPRO.
+=============================================================================
+
+Provides:
+1. Plots distributions of number and size
+2. Plots trajectories of dust events
+
+Subroutines:
+1. playvideo: displays a videofile
+2. playimages: plays a collection of image files
+3. area_distrib: displays information related to the pixel size of dust 
+                 clusters
+4. f_density_dist: displays number density per frame.
+5. plottraj: Plots trajectories of dust events
+
+=============================================================================
+
+Created on Tue June 11 2019
+
+@author: Arpan Khandelwal
+@author_email: napraarpan@gmail.com
+"""
+
+
 #Modules related to maily plotting and display of videos
-
+#######################################################
+##    Play image or video files                      ##
+#######################################################
+#play videofile
 from . import playvideo
-
+#play image collection
 from . import playimages
 
+#######################################################
+## Plot distributions                                ##
+#######################################################
+#plot size distributions
 from . import area_distrib
+#plot number distributions
+from . import f_density_dist
 
+#######################################################
+## Plot trajectories                                 ##
+#######################################################
+#plot trajectory
 from . import plottraj
+
