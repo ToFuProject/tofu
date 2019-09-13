7e0c32439509b8519daaf45749740e52e2cb11a4
Laura S. Mendoza <lasofivec@gmail.com>
2019-09-13 16:32:56 +0200

    Update conf.py
    changes for PEP8 speaks

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,9 +26,18 @@ sys.path.insert(0, os.path.abspath('/afs/ipp-garching.mpg.de/home/d/didiv/Python
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.napoleon', 'sphinx.ext.intersphinx',
-              'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig',
-              'sphinx.ext.viewcode', 'sphinx_gallery.gen_gallery']
+extensions = [
+  'sphinx.ext.autodoc',
+  'sphinx.ext.doctest',
+  'sphinx.ext.napoleon',
+  'sphinx.ext.intersphinx',
+  'sphinx.ext.todo',
+  'sphinx.ext.coverage',
+  'sphinx.ext.mathjax',
+  'sphinx.ext.ifconfig',
+  'sphinx.ext.viewcode',
+  'sphinx_gallery.gen_gallery'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
