cb2e2f5dba8558a6836b48e2a75d82c1e0166b24
Laura Mendoza <mendoza@math.unistra.fr>
2019-09-17 17:30:51 +0200

    [vmesh/debugging] update in vmesh test to find seg fault

--- tofu/tests/tests01_geom/vmesh_prueba.py
+++ tofu/tests/tests01_geom/vmesh_prueba.py
@@ -1,87 +1,208 @@
 # External modules
 import os
-import sys
 import numpy as np
 import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
-from matplotlib.path import Path
-import matplotlib.gridspec as mplgrid
-from mpl_toolkits.mplot3d import Axes3D
-import tofu as tf
-# Nose-specific
-from nose import with_setup # optional
-
-# ToFu-specific
+import tofu.geom as tfg
 import tofu.geom._GG as GG
 
+matplotlib.use("agg")
+# Nose-specific
+_here = os.path.abspath(os.path.dirname(__file__))
+VerbHead = "tofu.geom.tests03_core"
+keyVers = "Vers"
+_Exp = "WEST"
 
 
+def bigger_test():
+    """ exactly like test13_get_sampleV(self) """
 
-def main_test():
+    path = os.path.join(_here, "tests03_core_data")
+    lf = os.listdir(path)
+    lf = [f for f in lf if all([s in f for s in [_Exp, ".txt"]])]
+    lCls = sorted(set([f.split("_")[1] for f in lf]))
+    dobj = {"Tor": {}, "Lin": {}}
+    for tt in dobj.keys():
+        for cc in lCls:
+            lfc = [f for f in lf if f.split("_")[1] == cc and "V0" in f]
+            ln = []
+            for f in lfc:
+                if "CoilCS" in f:
+                    ln.append(f.split("_")[2].split(".")[0])
+                else:
+                    ln.append(f.split("_")[2].split(".")[0])
+            lnu = sorted(set(ln))
+            if not len(lnu) == len(ln):
+                msg = "Non-unique name list for {0}:".format(cc)
+                msg += "\n    ln = [{0}]".format(", ".join(ln))
+                msg += "\n    lnu = [{0}]".format(", ".join(lnu))
+                raise Exception(msg)
+            dobj[tt][cc] = {}
+            for ii in range(0, len(ln)):
+                if "BumperOuter" in ln[ii]:
+                    Lim = np.r_[10.0, 20.0] * np.pi / 180.0
+                elif "BumperInner" in ln[ii]:
+                    t0 = np.arange(0, 360, 60) * np.pi / 180.0
+                    Dt = 5.0 * np.pi / 180.0
+                    Lim = (
+                        t0[np.newaxis, :] + Dt * np.r_[-1.0, 1.0][:, np.newaxis]
+                    )  # noqa
+                elif "Ripple" in ln[ii]:
+                    t0 = np.arange(0, 360, 30) * np.pi / 180.0
+                    Dt = 2.5 * np.pi / 180.0
+                    Lim = (
+                        t0[np.newaxis, :] + Dt * np.r_[-1.0, 1.0][:, np.newaxis]
+                    )  # noqa
+                elif "IC" in ln[ii]:
+                    t0 = np.arange(0, 360, 120) * np.pi / 180.0
+                    Dt = 10.0 * np.pi / 180.0
+                    Lim = (
+                        t0[np.newaxis, :] + Dt * np.r_[-1.0, 1.0][:, np.newaxis]
+                    )  # noqa
+                elif "LH" in ln[ii]:
+                    t0 = np.arange(-180, 180, 120) * np.pi / 180.0
+                    Dt = 10.0 * np.pi / 180.0
+                    Lim = (
+                        t0[np.newaxis, :] + Dt * np.r_[-1.0, 1.0][:, np.newaxis]
+                    )  # noqa
+                elif tt == "Lin":
+                    Lim = np.r_[0.0, 10.0]
+                else:
+                    Lim = None
+                Poly = np.loadtxt(os.path.join(path, lfc[ii]))
+                assert Poly.ndim == 2
+                assert Poly.size >= 2 * 3
+                kwd = dict(  # noqa
+                    Name=ln[ii] + tt,
+                    Exp=_Exp,
+                    SavePath=_here,
+                    Poly=Poly,
+                    Lim=Lim,
+                    Type=tt,
+                )  # noqa
+                dobj[tt][cc][ln[ii]] = eval("tfg.%s(**kwd)" % cc)
+    for typ in dobj.keys():
+        # Todo : introduce possibility of choosing In coordinates !
+        for c in dobj[typ].keys():
+            if issubclass(eval("tfg.%s" % c), tfg._core.StructOut):
+                continue
+            for n in dobj[typ][c].keys():
+                obj = dobj[typ][c][n]
+                box = None  # [[2.,3.], [0.,5.], [0.,np.pi/2.]]
+                try:
+                    ii = 0
+                    print(
+                        ".............. about to try with :", ii, n, box, "abs"
+                    )  # noqa
+                    out = obj.get_sampleV(
+                        0.1, resMode="abs", DV=box, Out="(X,Y,Z)"
+                    )  # noqa
+                    print("done")
+                    pts0, ind = out[0], out[2]
+                    ii = 1
+                    print(
+                        ".............. about to try with :", ii, n, ind, "abs"
+                    )  # noqa
+                    out = obj.get_sampleV(
+                        0.1, resMode="abs", ind=ind, Out="(X,Y,Z)"
+                    )  # noqa
+                    print("done")
+                    pts1 = out[0]
+                except Exception as err:
+                    msg = str(err)
+                    msg += "\nFailed for {0}_{1}_{2}".format(typ, c, n)
+                    msg += "\n    ii={0}".format(ii)
+                    msg += "\n    Lim={0}".format(str(obj.Lim))
+                    msg += "\n    DS={0}".format(str(box))
+                    raise Exception(msg)
+
+                if type(pts0) is list:
+                    assert all(
+                        [
+                            np.allclose(pts0[ii], pts1[ii])
+                            for ii in range(0, len(pts0))
+                        ]  # noqa
+                    )
+                else:
+                    assert np.allclose(pts0, pts1)
+
+
+def small_test():
+    """Test vmesh"""
+
     # VPoly
-    thet = np.linspace(0.,2.*np.pi,100)
-    VPoly = np.array([2.+1.*np.cos(thet), 0.+1.*np.sin(thet)])
-    
-    
-    RMinMax = np.array([np.min(VPoly[0,:]), np.max(VPoly[0,:])])
-    ZMinMax = np.array([np.min(VPoly[1,:]), np.max(VPoly[1,:])])
+    thet = np.linspace(0.0, 2.0 * np.pi, 100)
+    VPoly = np.array([2.0 + 1.0 * np.cos(thet), 0.0 + 1.0 * np.sin(thet)])
+    RMinMax = np.array([np.min(VPoly[0, :]), np.max(VPoly[0, :])])
+    ZMinMax = np.array([np.min(VPoly[1, :]), np.max(VPoly[1, :])])
     dR, dZ, dRPhi = 0.05, 0.05, 0.05
-    LDPhi = [None, [3.*np.pi/4.,5.*np.pi/4.], [-np.pi/4.,np.pi/4.]]
-    
-    for ii in range(0,len(LDPhi)):
-        Pts, dV, ind, \
-            dRr, dZr, dRPhir = GG._Ves_Vmesh_Tor_SubFromD_cython(dR, dZ, dRPhi,
-                                                                 RMinMax,
-                                                                 ZMinMax,
-                                                                 DR=np.array([0.5,2.]),
-                                                                 DZ=np.array([0.,1.2]),
-                                                                 DPhi=LDPhi[ii],
-                                                                 VPoly=VPoly,
-                                                                 Out='(R,Z,Phi)',
-                                                                 margin=1.e-9)
-        assert Pts.ndim==2 and Pts.shape[0]==3
-        assert np.all(Pts[0,:]>=1.)
-        assert np.all(Pts[0,:]<=2.)
-        assert np.all(Pts[1,:]>=0.)
-        assert np.all(Pts[1,:]<=1.)
-        marg = np.abs(np.arctan(np.mean(dRPhir)/np.min(VPoly[1,:])))
+    LDPhi = [None, [3.0 * np.pi / 4.0, 5.0 * np.pi / 4.0], [-np.pi / 4.0, np.pi / 4.0]]  # noqa
+    for ii in range(0, len(LDPhi)):
+        Pts, dV, ind, dRr, dZr, dRPhir = GG._Ves_Vmesh_Tor_SubFromD_cython(
+            dR,
+            dZ,
+            dRPhi,
+            RMinMax,
+            ZMinMax,
+            DR=np.array([0.5, 2.0]),
+            DZ=np.array([0.0, 1.2]),
+            DPhi=LDPhi[ii],
+            VPoly=VPoly,
+            Out="(R,Z,Phi)",
+            margin=1.0e-9,
+        )
+        assert Pts.ndim == 2 and Pts.shape[0] == 3
+        assert np.all(Pts[0, :] >= 1.0)
+        assert np.all(Pts[0, :] <= 2.0)
+        assert np.all(Pts[1, :] >= 0.0)
+        assert np.all(Pts[1, :] <= 1.0)
+        marg = np.abs(np.arctan(np.mean(dRPhir) / np.min(VPoly[1, :])))
         if not LDPhi[ii] is None:
-            LDPhi[ii][0] = np.arctan2(np.sin(LDPhi[ii][0]),np.cos(LDPhi[ii][0]))
-            LDPhi[ii][1] = np.arctan2(np.sin(LDPhi[ii][1]),np.cos(LDPhi[ii][1]))
-            if LDPhi[ii][0]<=LDPhi[ii][1]:
-                assert np.all((Pts[2,:]>=LDPhi[ii][0]-marg) &
-                              (Pts[2,:]<=LDPhi[ii][1]+marg))
+            LDPhi[ii][0] = np.arctan2(
+                np.sin(LDPhi[ii][0]), np.cos(LDPhi[ii][0])
+            )  # noqa
+            LDPhi[ii][1] = np.arctan2(
+                np.sin(LDPhi[ii][1]), np.cos(LDPhi[ii][1])
+            )  # noqa
+            if LDPhi[ii][0] <= LDPhi[ii][1]:
+                assert np.all(
+                    (Pts[2, :] >= LDPhi[ii][0] - marg)
+                    & (Pts[2, :] <= LDPhi[ii][1] + marg)
+                )
             else:
-                assert np.all( (Pts[2,:]>=LDPhi[ii][0]-marg) |
-                               (Pts[2,:]<=LDPhi[ii][1]+marg))
-        assert dV.shape==(Pts.shape[1],)
+                assert np.all(
+                    (Pts[2, :] >= LDPhi[ii][0] - marg)
+                    | (Pts[2, :] <= LDPhi[ii][1] + marg)
+                )
+        assert dV.shape == (Pts.shape[1],)
         assert ind.shape == (Pts.shape[1],)
-        assert ind.dtype==int
-        assert np.unique(ind).size==ind.size
-        assert np.all(ind==np.unique(ind))
-        assert np.all(ind>=0)
-        assert all([ind.shape==(Pts.shape[1],),
-                    ind.dtype==int,
-                    np.unique(ind).size==ind.size,
-                    np.all(ind==np.unique(ind)),
-                    np.all(ind>=0)])
-        assert dRPhir.ndim==1
-        Ptsi, \
-            dVi, dRri, dZri,\
-            dRPhiri = GG._Ves_Vmesh_Tor_SubFromInd_cython(dR, dZ, dRPhi,
-                                                          RMinMax, ZMinMax, ind,
-                                                          Out='(R,Z,Phi)',
-                                                          margin=1.e-9)
-        assert np.allclose(Pts,Ptsi)
-        assert np.allclose(dV,dVi)
-        assert dRr==dRri and dZr==dZri
+        assert ind.dtype == int
+        assert np.unique(ind).size == ind.size
+        assert np.all(ind == np.unique(ind))
+        assert np.all(ind >= 0)
+        assert all(
+            [
+                ind.shape == (Pts.shape[1],),
+                ind.dtype == int,
+                np.unique(ind).size == ind.size,
+                np.all(ind == np.unique(ind)),
+                np.all(ind >= 0),
+            ]
+        )
+        assert dRPhir.ndim == 1
+        Ptsi, dVi, dRri, dZri, dRPhiri = GG._Ves_Vmesh_Tor_SubFromInd_cython(
+            dR, dZ, dRPhi, RMinMax, ZMinMax, ind, Out="(R,Z,Phi)", margin=1.0e-9
+        )
+        assert np.allclose(Pts, Ptsi)
+        assert np.allclose(dV, dVi)
+        assert dRr == dRri and dZr == dZri
         assert np.allclose(dRPhir, dRPhiri)
 
 
-
 if __name__ == "__main__":
-    import timeit
-    print(timeit.timeit("main_test()", setup="from __main__ import main_test", number=500))
-
-    
+    # import timeit
+    # print("small test ran in =",
+    #       timeit.timeit("small_test()",
+    #                     setup="from __main__ import small_test",
+    #                     number=500))
+    print(".-.-.-.-.-.-.-. Bigger test .-.-.-.-.-.-.-.-")
+    bigger_test()
