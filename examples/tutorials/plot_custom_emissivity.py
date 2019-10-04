e942045ea3f8f4aa01ecf3991f80ee44c8606b43
Florian Le Bourdais <florian.s.lebourdais@gmail.com>
2019-10-04 17:47:12 +0200

    Update plot_custom_emissivity.py
    better plots

--- examples/tutorials/plot_custom_emissivity.py
+++ examples/tutorials/plot_custom_emissivity.py
@@ -30,14 +30,19 @@ cam2d = tf.geom.utils.create_CamLOS2D(
 ###############################################################################
 # Now, we define an emissivity function that depends on r and z coordinates.
 # We can plot its profile in a section.
-
+import numpy as np
 import matplotlib.pyplot as plt
 
 
 def emissivity(pts, t=None, vect=None):
     """Custom emissivity as a function of geometry.
 
-    pts is a tuple holding x, y, z geometry coordinates.
+
+    :param pts: ndarray of shape (3, n_points) (each column is a xyz coordinate)
+    :param t: optional, time parameter to add a time dependency to the emissivity function
+    :param vect:
+    :return:
+        - emissivity - array holding the emissivity for each point in the input grid
     """
     r, z = np.hypot(pts[0, :], pts[1, :]), pts[2, :]
     e = np.exp(-(r - 2.4) ** 2 / 0.2 ** 2 - z ** 2 / 0.2 ** 2)
@@ -46,8 +51,8 @@ def emissivity(pts, t=None, vect=None):
     return e
 
 
-y = np.linspace(-10, 10)
-z = np.linspace(-10, 12)
+y = np.linspace(2, 3, num=90)
+z = np.linspace(-0.5, 0.5, num=100)
 Y, Z = np.meshgrid(y, z)
 X = np.zeros_like(Y)
 pts = np.c_[X.ravel(), Y.ravel(), Z.ravel()].T
@@ -55,16 +60,18 @@ emissivity_vals = emissivity(pts)
 emissivity_vals = emissivity_vals.reshape(X.shape)
 
 fig, ax = plt.subplots()
-ax.pcolormesh(emissivity_vals)
+ax.pcolormesh(Y, Z, emissivity_vals)
+ax.set_xlabel('y')
+ax.set_ylabel('z')
 plt.show()
 
 ###############################################################################
 # Finally, we compute an image using the 2D camera and this emissivity. We use the
 # `plot=True` flag to obtain a graphical output.
 
-time_vector = np.linspace(0, 20, num=1000)
+time_vector = np.linspace(0, 2 * np.pi, num=100)
 
 sig, units = cam2d.calc_signal(emissivity,
-                               resMode='abs', plot=False,
+                               resMode='rel', plot=False,
                                t=time_vector)
 sig.plot()
