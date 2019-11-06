bb8a2dcccc46cdfd4ffb52034b1e1cdff793e8fb
Jorge Morales <jorge.morales2@cea.fr>
2019-11-06 12:01:13 +0100

    -Avoid to change the sys path if test files are loaded from python console

--- tofu/mag/test_magFieldLines.py
+++ tofu/mag/test_magFieldLines.py
@@ -10,11 +10,12 @@ import doctest
 import os
 import sys
 
-#print('path 1 =', sys.path)
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-#print('path 2 =', sys.path)
-
-#import mag
-from mag import magFieldLines
-
-doctest.testmod(magFieldLines, verbose=True)
+if __name__ == '__main__':
+    #print('path 1 =', sys.path)
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+    #print('path 2 =', sys.path)
+    
+    #import mag
+    from mag import magFieldLines
+    
+    doctest.testmod(magFieldLines, verbose=True)
