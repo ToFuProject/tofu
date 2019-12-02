98b6fd81c642c204a13943d19ba480015603d3f2
Laura S. Mendoza <laura.mendoza@inria.fr>
2019-12-02 01:41:12 +0100

    [pypi] exported path correctly

--- conda_recipe/conda_upload.sh
+++ conda_recipe/conda_upload.sh
@@ -1,11 +1,6 @@
 # Only need to change these two variables
-PKG_NAME=tofu
 USER=ToFuProject
 
-conda config --set anaconda_upload no
-conda install anaconda-client conda-build
-conda build conda_recipe
-export PKG_REAL=$(conda build . --output | tail -1)
 echo "Available conda packages:"
 echo $PKG_REAL
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $PKG_REAL --force
