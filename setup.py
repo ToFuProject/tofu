91b686e70bd93c523184703e2fb5c6cdf538d07d
Laura Mendoza <mendoza@math.unistra.fr>
2020-02-06 16:15:04 +0100

    erased old update version function in setup

--- setup.py
+++ setup.py
@@ -16,10 +16,7 @@ from setuptools import Extension
 # ... packages that need to be in pyproject.toml
 from Cython.Distutils import build_ext
 import numpy as np
-# ...
-# Remove _updateversion import due to build-time dependency
-#   => updateversion defined in here for commodity
-#   => find a cleaner solution later
+# ... local script
 import _updateversion as up
 # ... for `clean` command
 from distutils.command.clean import clean as Clean
@@ -147,25 +144,6 @@ else:
 
 # == Getting tofu version =====================================================
 _HERE = os.path.abspath(os.path.dirname(__file__))
-
-
-# def updateversion(path=_HERE):
-#     # Fetch version from git tags, and write to version.py
-#     # Also, when git is not available (PyPi package), use stored version.py
-#     version_py = os.path.join(path, 'tofu', 'version.py')
-#     try:
-#         version_git = subprocess.check_output(["git",
-#                                                "describe"]).rstrip().decode()
-#     except subprocess.CalledProcessError:
-#         with open(version_py, 'r') as fh:
-#             version_git = fh.read().strip().split("=")[-1].replace("'", '')
-#     version_git = version_git.lower().replace('v', '')
-
-#     version_msg = "# Do not edit, pipeline versioning governed by git tags!"
-#     with open(version_py, "w") as fh:
-#         msg = "{0}__version__ = '{1}'{0}".format(os.linesep, version_git)
-#         fh.write(version_msg + msg)
-#     return version_git
 
 
 def get_version_tofu(path=_HERE):
