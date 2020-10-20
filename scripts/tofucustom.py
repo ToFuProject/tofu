827b989c52fd83870e184c8dab2474708c5976d4
VEZINET Didier <didier.vezinet@cea.fr>
2020-10-20 09:59:15 +0200

    [Issue416] Added sys.path.append() in tofuplot and tofucustom

--- scripts/tofucustom.py
+++ scripts/tofucustom.py
@@ -5,8 +5,13 @@ import os
 from shutil import copyfile
 
 
+_HERE = os.path.abspath(os.path.dirname(__file__))
+
+
 # import parser dict
+sys.path.insert(1, _HERE)
 from _dparser import _DPARSER
+_ = sys.path.pop(1)
 
 
 ###################################################
--- scripts/tofuversion.py
+++ scripts/tofuversion.py
@@ -5,8 +5,13 @@ import os
 import warnings
 
 
+_HERE = os.path.abspath(os.path.dirname(__file__))
+
+
 # import parser dict
+sys.path.insert(1, _HERE)
 from _dparser import _DPARSER
+_ = sys.path.pop(1)
 
 
 ###################################################
