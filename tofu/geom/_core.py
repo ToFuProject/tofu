[Issue465_WrongStructIndexingInCamPlotTouch] Corrected bug, was due to wrong numbering in preparation of lStruct for kinout computation

tofu/geom/_core.py
@@ -4770,29 +4770,42 @@ class Rays(utils.ToFuObject):
 
         # Get reference: lS
         if indStruct is None:
-            indStruct = self.indStruct_computeInOut
-        lS = [
-            ss for ii, ss in enumerate(self.config.lStruct) if ii in indStruct
-        ]
+            indIn, indOut = self.get_indStruct_computeInOut(unique_In=True)
+            indStruct = np.r_[indIn, indOut]
+        else:
+            indIn = [
+                ii for ii in indStruct
+                if self.config.lStruct[ii]._InOut == "in"
+            ]
+            if len(indIn) > 1:
+                ind = np.argmin([
+                    self.config.lStruct[ii].dgeom['Surf']
+                    for ii in indIn
+                ])
+                indStruct = [ii for ii in indStruct
+                             if ii not in indIn or ii == ind]
+                indIn = [indIn[ind]]
+            indOut = [
+                ii for ii in indStruct
+                if self.config.lStruct[ii]._InOut == "out"
+            ]
 
-        lSIn = [ss for ss in lS if ss._InOut == "in"]
-        if len(lSIn) == 0:
+        if len(indIn) == 0:
             msg = "self.config must have at least a StructIn subclass !"
-            assert len(lSIn) > 0, msg
-        iref = np.argmin([ss.dgeom["Surf"] for ss in lSIn])
-        S = lSIn[iref]
+            raise Exception(msg)
 
+        S = self.config.lStruct[indIn[0]]
         VPoly = S.Poly_closed
         VVIn = S.dgeom["VIn"]
         largs = [D, u, VPoly, VVIn]
 
+        lS = [self.config.lStruct[ii] for ii in indOut]
         if self._method == "ref":
 
             Lim = S.Lim
             nLim = S.noccur
             VType = self.config.Id.Type
 
-            lS = [ss for ss in lS if ss._InOut == "out"]
             lSPoly, lSVIn, lSLim, lSnLim = [], [], [], []
             for ss in lS:
                 lSPoly.append(ss.Poly_closed)
@@ -4826,7 +4839,6 @@ class Rays(utils.ToFuObject):
             nLim = S.noccur
             VType = self.config.Id.Type
 
-            lS = [ss for ss in lS if ss._InOut == "out"]
             lSPolyx, lSVInx = [], []
             lSPolyy, lSVIny = [], []
             lSLim, lSnLim = [], []
@@ -5560,19 +5572,6 @@ class Rays(utils.ToFuObject):
         return self._dconfig["Config"]
 
     @property
-    def indStruct_computeInOut(self):
-        compute = self.config.get_compute()
-        lS = self.config.lStruct
-        iI, iO = [], []
-        for ii in range(0, len(lS)):
-            if compute[ii]:
-                if lS[ii]._InOut == "in":
-                    iI.append(ii)
-                elif lS[ii]._InOut == "out":
-                    iO.append(ii)
-        return np.r_[iI + iO]
-
-    @property
     def Etendues(self):
         if self._dgeom["Etendues"] is None:
             E = None
@@ -5824,6 +5823,33 @@ class Rays(utils.ToFuObject):
     ###########
     # public methods
     ###########
+
+    def get_indStruct_computeInOut(self, unique_In=None):
+        """ The indices of structures with compute = True
+
+        The indidces refer to self.config.lStruct
+            - The first array corresponds to Struct of type In
+            - The second array corresponds to Struct of type Out
+        """
+        if unique_In is None:
+            unique_In = False
+
+        compute = self.config.get_compute()
+        indIn = np.array([
+            ii for ii, ss in enumerate(self.config.lStruct)
+            if compute[ii] and ss._InOut == "in"
+        ], dtype=int)
+        if unique_In is True and indIn.size > 1:
+            iind = np.argmin([
+                self.config.lStruct[ii].dgeom['Surf'] for ii in indIn
+            ])
+            indIn = np.r_[indIn[iind]]
+
+        indOut = np.array([
+            ii for ii, ss in enumerate(self.config.lStruct)
+            if compute[ii] and ss._InOut == "out"
+        ], dtype=int)
+        return indIn, indOut
 
     def _check_indch(self, ind, out=int):
         if ind is not None:
@@ -7386,8 +7412,8 @@ class Rays(utils.ToFuObject):
         dElt = {}
         lS = self.config.lStruct
         ind = self._check_indch(ind, out=bool)
-        for ii in self.indStruct_computeInOut:
-            kn = "%s_%s" % (lS[ii].__class__.__name__, lS[ii].Id.Name)
+        for ii in np.r_[self.get_indStruct_computeInOut(unique_In=True)]:
+            kn = "{}_{}".format(lS[ii].__class__.__name__, lS[ii].Id.Name)
             indtouch = self.select(touch=kn, out=bool)
             if np.any(indtouch):
                 indok = indtouch & ind
@@ -7404,8 +7430,13 @@ class Rays(utils.ToFuObject):
         return dElt
 
     def get_touch_colors(
-        self, ind=None, dElt=None, cbck=(0.8, 0.8, 0.8), rgba=True
+        self,
+        ind=None,
+        dElt=None,
+        cbck=(0.8, 0.8, 0.8),
+        rgba=True,
     ):
+        """ Get array of colors per LOS (color set by the touched Struct) """
         if dElt is None:
             dElt = self.get_touch_dict(ind=None, out=bool)
         else: