492068d76f80657af1bd69edc441e14a7adbb0c0
Didou09 <didier.vezineT@gmail.com>
2021-05-07 23:10:24 +0200

    [#208] Plasma2D (new) not operational yet => removed until completion

--- tofu/data/_plasma.py
+++ tofu/data/_plasma.py
@@ -19,20 +19,20 @@ __all__ = ['Plasma2D']
 
 
 
-class Plasma2D(DataCollection):
+# class Plasma2D(DataCollection):
 
-    _ddef = {'Id': {'include': ['Mod', 'Cls', 'Name', 'version']},
-             'params': {'origin': (str, 'unknown'),
-                        'dim':    (str, 'unknown'),
-                        'quant':  (str, 'unknown'),
-                        'name':   (str, 'unknown'),
-                        'units':  (str, 'a.u.')}}
-    _forced_group = ['time', 'radius', 'mesh2d']
-    _data_none = False
+    # _ddef = {'Id': {'include': ['Mod', 'Cls', 'Name', 'version']},
+             # 'params': {'origin': (str, 'unknown'),
+                        # 'dim':    (str, 'unknown'),
+                        # 'quant':  (str, 'unknown'),
+                        # 'name':   (str, 'unknown'),
+                        # 'units':  (str, 'a.u.')}}
+    # _forced_group = ['time', 'radius', 'mesh2d']
+    # _data_none = False
 
-    _show_in_summary_core = ['shape', 'ref', 'group']
-    _show_in_summary = 'all'
-    _max_ndim = 2
+    # _show_in_summary_core = ['shape', 'ref', 'group']
+    # _show_in_summary = 'all'
+    # _max_ndim = 2
 
 
 
