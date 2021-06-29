b68de5c3cabbcc8009c4a29c5f0c50578840c6f5
Laura S. Mendoza <laura.mendoza@inria.fr>
2021-06-29 13:27:31 +0200

    added video on tutorial and added it to doc

---
+++ examples/tutorials/tuto_plot_create_geometry_from_svg.py
@@ -0,0 +1,57 @@
+"""
+Creating a geometry with Inkscape
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+This is a tutorial that shows you how to draw a geometry in Inkscape
+and load it in tofu.
+"""
+
+###############################################################################
+# To see a tutorial on how to draw a vessel's geometry on Inkscape and save
+# it to a `svg` file, you can check the video below. Basically, you just need
+# to use straight *Bezier curves* to draw the closed polygons that will define
+# the vessel and optionally the structures. To define a PFC structure, just
+# add a fill color.
+
+###############################################################################
+
+###############################################################################
+# .. raw:: html
+#
+#    <div class="text-center">
+#    <iframe width="560" height="315"
+#    src="https://www.youtube.com/embed/BoZh2U7I8-g"
+#    title="YouTube video player"
+#    frameborder="0" allow="accelerometer; autoplay; clipboard-write;
+#    encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+#    </iframe>
+#    </div>
+
+###############################################################################
+# Now, on how to load it and use it in tofu (also shown in the video). We start
+# by importing `tofu`.
+
+import tofu as tf
+
+###############################################################################
+# `tofu` provides a geometry helper function that allows creating a
+# configuration from a `svg` file. Supposing you saved it in a file named
+# `"myfirstgeom.svg"`, do
+config = tf.geom.Config.from_svg("myfirstgeom.svg", Name="Test", Exp="Test")
+
+###############################################################################
+# It already gives you some information on what you loaded, but to see what it
+# actually contains:
+print(config)
+
+###############################################################################
+# To plot it, simply do
+config.plot()
+
+###############################################################################
+# We can see that the z- and r-axis might not be exactly what we wanted. We
+# might also want to adjust the scale. Tofu let's fix those parameters
+# when loading the configuration.
+config = tf.geom.Config.from_svg("myfirstgeom.svg", Name="Test", Exp="Test",
+                                 z0=-140, r0=100, scale=0.5)
+config.plot()
--- tofu/version.py
+++ tofu/version.py
@@ -1,3 +1,2 @@
 # Do not edit, pipeline versioning governed by git tags!
 __version__ = '1.4.10-alpha9-335-g1e388929'
-
