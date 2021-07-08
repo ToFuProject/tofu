55cd091e6ba2a53f00b3616398f86d19b4787ed6
VEZINET Didier <didier.vezinet@cea.fr>
2021-07-08 12:23:23 +0200

    [#202] PEP8 compliance 7

--- tofu/tests/tests01_geom/test_04_core_optics.py
+++ tofu/tests/tests01_geom/test_04_core_optics.py
@@ -33,51 +33,52 @@ def setup_module(module):
     print("") # this is to get a newline after the dots
     lf = os.listdir(_here)
     lf = [f for f in lf
-         if all([s in f for s in ['TFG_',_Exp,'.npz']])]
+         if all([s in f for s in ['TFG_', _Exp, '.npz']])]
     lF = []
     for f in lf:
         ff = f.split('_')
         v = [fff[len(keyVers):] for fff in ff
-             if fff[:len(keyVers)]==keyVers]
+             if fff[:len(keyVers)] == keyVers]
         msg = f + "\n    "+str(ff) + "\n    " + str(v)
-        assert len(v)==1, msg
+        assert len(v) == 1, msg
         v = v[0]
         if '.npz' in v:
             v = v[:v.index('.npz')]
         # print(v, __version__)
-        if v!=__version__:
+        if v != __version__:
             lF.append(f)
-    if len(lF)>0:
+    if len(lF) > 0:
         print("Removing the following previous test files:")
         for f in lF:
-            os.remove(os.path.join(_here,f))
-        #print("setup_module before anything in this file")
+            os.remove(os.path.join(_here, f))
+        # print("setup_module before anything in this file")
+
 
 def teardown_module(module):
-    #os.remove(VesTor.Id.SavePath + VesTor.Id.SaveName + '.npz')
-    #os.remove(VesLin.Id.SavePath + VesLin.Id.SaveName + '.npz')
-    #print("teardown_module after everything in this file")
-    #print("") # this is to get a newline
+    # os.remove(VesTor.Id.SavePath + VesTor.Id.SaveName + '.npz')
+    # os.remove(VesLin.Id.SavePath + VesLin.Id.SaveName + '.npz')
+    # print("teardown_module after everything in this file")
+    # print("") # this is to get a newline
     lf = os.listdir(_here)
     lf = [f for f in lf
-         if all([s in f for s in ['TFG_',_Exp,'.npz']])]
+         if all([s in f for s in ['TFG_', _Exp, '.npz']])]
     lF = []
     for f in lf:
         ff = f.split('_')
         v = [fff[len(keyVers):] for fff in ff
-             if fff[:len(keyVers)]==keyVers]
+             if fff[:len(keyVers)] == keyVers]
         msg = f + "\n    "+str(ff) + "\n    " + str(v)
-        assert len(v)==1, msg
+        assert len(v) == 1, msg
         v = v[0]
         if '.npz' in v:
             v = v[:v.index('.npz')]
         # print(v, __version__)
-        if v==__version__:
+        if v == __version__:
             lF.append(f)
-    if len(lF)>0:
+    if len(lF) > 0:
         print("Removing the following test files:")
         for f in lF:
-            os.remove(os.path.join(_here,f))
+            os.remove(os.path.join(_here, f))
 
 
 #######################################################
@@ -90,32 +91,34 @@ class Test01_Crystal(object):
 
     @classmethod
     def setup_class(cls, verb=False):
-        #print ("")
-        #print "--------- "+VerbHead+cls.__name__
+        # print ("")
+        # print "--------- "+VerbHead+cls.__name__
 
         # Prepare input
         dgeom = {
             'Type': 'sph',
             'Typeoutline': 'rect',
-            'summit': np.array([ 4.6497750e-01, -8.8277925e+00,  3.5125000e-03]),
-            'center': np.array([ 1.560921  , -6.31106476,  0.00729429]),
+            'summit': np.array([4.6497750e-01, -8.8277925e+00, 3.5125000e-03]),
+            'center': np.array([1.560921, -6.31106476, 0.00729429]),
             'extenthalf': np.array([0.01457195, 0.01821494]),
             'rcurve': 2.745,
             'move': 'rotate_around_3daxis',
             'move_param': 0.022889993139905633,
             'move_kwdargs': {
-                'axis': np.array([[ 4.95e-01, -8.95e+00, -8.63e-02],
-                               [-1.37e-04, -2.18e-03,  9.99e-01]])
+                'axis': np.array([
+                    [4.95e-01, -8.95e+00, -8.63e-02],
+                    [-1.37e-04, -2.18e-03,  9.99e-01],
+                ])
             }
         }
         dmat = {
             'formula': 'Quartz',
-             'density': 2.6576,
-             'symmetry': 'hexagonal',
-             'lengths': np.array([4.9079e-10, 4.9079e-10, 5.3991e-10]),
-             'angles': np.array([1.57079633, 1.57079633, 2.0943951 ]),
-             'cut': np.array([ 1,  1, -2,  0]),
-             'd': 2.4539499999999996e-10,
+            'density': 2.6576,
+            'symmetry': 'hexagonal',
+            'lengths': np.array([4.9079e-10, 4.9079e-10, 5.3991e-10]),
+            'angles': np.array([1.57079633, 1.57079633, 2.0943951]),
+            'cut': np.array([1,  1, -2,  0]),
+            'd': 2.4539499999999996e-10,
         }
         dbragg = {
             'lambref': 3.96e-10,
@@ -181,27 +184,16 @@ class Test01_Crystal(object):
         cls.xi = 0.05*np.linspace(-1, 1, 100)
         cls.xj = 0.10*np.linspace(-1, 1, 200)
 
-
     @classmethod
     def teardown_class(cls):
-        #print ("teardown_class() after any methods in this class")
         pass
-        # for typ in cls.dobj.keys():
-            # for c in cls.dobj[typ].keys():
-                # for f in cls.dlpfe[typ][c]:
-                    # try:
-                        # os.remove(f)
-                    # except Exception as err:
-                        # msg = str(err)
-                        # msg += '\n\n'+str(f)
-                        # raise Exception(msg)
 
     def setup(self):
-        #print ("TestUM:setup() before each test method")
+        # print ("TestUM:setup() before each test method")
         pass
 
     def teardown(self):
-        #print ("TestUM:teardown() after each test method")
+        # print ("TestUM:teardown() after each test method")
         pass
 
     # def test00_todo(self):
@@ -222,7 +214,7 @@ class Test01_Crystal(object):
         for k0 in self.dobj.keys():
             obj = self.dobj[k0].copy()
             assert obj == self.dobj[k0]
-            assert not  obj != self.dobj[k0]
+            assert not obj != self.dobj[k0]
 
     def test04_get_nbytes(self):
         for k0 in self.dobj.keys():
@@ -244,14 +236,6 @@ class Test01_Crystal(object):
 
     def test07_rotate_copy(self):
         pass
-        # for typ in self.dobj.keys():
-            # if typ == 'Lin':
-                # continue
-            # dkwd0 = dict(axis_rz=[2.4, 0], angle=np.pi/4,
-                         # return_copy=True)
-            # for c in self.dobj[typ].keys():
-                # obj = getattr(self.dobj[typ][c],
-                              # 'rotate_in_cross_section')(**dkwd0)
 
     def test08_get_detector_approx(self):
         for k0, obj in self.dobj.items():
@@ -292,7 +276,7 @@ class Test01_Crystal(object):
                 pts=pts,
                 lamb=lamb,
                 det=det,
-                rays_color='pts' if ii%2 == 0 else 'lamb',
+                rays_color='pts' if ii % 2 == 0 else 'lamb',
             )
             ii += 1
         plt.close('all')
--- tofu/version.py
+++ tofu/version.py
@@ -1,2 +1,2 @@
 # Do not edit, pipeline versioning governed by git tags!
-__version__ = '1.4.10-alpha9-719-g1c2f0731'
+__version__ = '1.4.10-alpha9-720-gc8792758'
