36b28b7a7f1060a8bde3092416f7f197174640cc
VEZINET Didier <didier.vezinet@cea.fr>
2021-11-05 16:12:35 +0100

    [#572] First version of benchmarks for Geometry routines

--- tofu/benchmarks/benchmarks.py
+++ tofu/benchmarks/benchmarks.py
@@ -2,35 +2,97 @@
 # See "Writing benchmarks" in the asv docs for more information.
 
 
-class TimeSuite:
+import tofu as tf
+
+
+class Geometry:
+    """ Benchmark the geometry-oriented routines
+
+    In particular:
+        - camera creation (ray-tracing)
+        - solid angle computing
+
     """
-    An example benchmark that times the performance of various kinds
-    of iterating over dictionaries in Python.
-    """
+
+    # -----------------------------
+    # Attributes reckognized by asv
+
+    # time before benchmark is killed
+    timeout = 60
+
+    # -------------------------------------------------------
+    # Setup and teardown, run before / after benchmark methods
+
     def setup(self):
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
 
-    def time_keys(self):
-        for key in self.d.keys():
-            pass
+        # Load a configuration
+        self.conf = tf.load_config('ITER')
 
-    def time_iterkeys(self):
-        for key in self.d.iterkeys():
-            pass
+        # prepare input dict for a cam1d
+        self.dcam1d = {
+            'pinhole': [8.38/np.sqrt(2.), 8.38/np.sqrt(2.), 0.],
+            'orientation': [-np.pi, 0., 0],
+            'focal': 0.08,
+            'sensor_nb': 100,
+            'sensor_size': 0.3,
+            'config': self.conf,
+            'Diag': 'SXR',
+            'Exp': 'WEST',
+            'Name': 'cam1',
+        }
 
-    def time_range(self):
-        d = self.d
-        for key in range(500):
-            x = d[key]
+        # prepare input dict for a cam2d
+        self.dcam2d = {
+            'pinhole': [8.38, 0., 0.],
+            'orientation': [-7*np.pi/8, np.pi/6, 0],
+            'focal': 0.08,
+            'sensor_nb': 400,
+            'sensor_size': 0.2,
+            'config': self.conf,
+            'Diag': 'SXR',
+            'Exp': 'WEST',
+            'Name': 'cam2',
+        }
 
-    def time_xrange(self):
-        d = self.d
-        for key in xrange(500):
-            x = d[key]
+        # prepare input dict for particle solid angle toroidal integration
+        self.dpart = {
+            'part_traj': np.array([
+                [6., 0., 0.], [6., 0.01, -4],
+            ]).T,
+            'part_radius': np.array([10e-6, 10e-6]),
+            'resolution': 0.3,
+            'DPhi': [np.pi/2, np.pi/2]
+            'vmax': False,
+            'approx': False,
+        }
 
+    def teardown(self):
+        pass
 
-class MemSuite:
-    def mem_list(self):
-        return [0] * 256
+    # -------------------------------------
+    # benchmarks methods for geometry tools
+
+    # CAMLOS1D
+    def time_camlos1d(self):
+        cam = tf.geom.utils.create_CamLOS1D(**self.dcam1d)
+
+    def peakmem_camlos1d(self):
+        cam = tf.geom.utils.create_CamLOS1D(**self.dcam1d)
+
+    # CAMLOS2D
+    def time_camlos2d(self):
+        cam = tf.geom.utils.create_CamLOS2D(**self.dcam2d)
+
+    def peakmem_camlos2d(self):
+        cam = tf.geom.utils.create_CamLOS2D(**self.dcam2d)
+
+    # Solid angle toroidal integral for particle
+    def time_solidangle_part(self):
+        (
+            ptsRZ, sang, indices, reseff, dax,
+        ) = self.conf.calc_solidangle_particle_integrated(**self.dpart)
+
+    def peakmem_solidangle_part(self):
+        (
+            ptsRZ, sang, indices, reseff, dax,
+        ) = self.conf.calc_solidangle_particle_integrated(**self.dpart)
--- tofu/version.py
+++ tofu/version.py
@@ -1,2 +1,2 @@
 # Do not edit, pipeline versioning governed by git tags!
-__version__ = '1.4.15-95-g232dd5be'
+__version__ = '1.4.15-96-g87ceeb65'
