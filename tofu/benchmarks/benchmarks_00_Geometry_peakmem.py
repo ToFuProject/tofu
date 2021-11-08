59fc4afda0ac12b3a5668eda01316fdcb33514f5
Didou09 <didier.vezineT@gmail.com>
2021-11-08 16:15:17 +0100

    [#572] Done with Geometry reformatting, working

--- tofu/benchmarks/benchmarks_00_Geometry_peakmem.py
+++ tofu/benchmarks/benchmarks_00_Geometry_peakmem.py
@@ -16,7 +16,7 @@ import tofu as tf
 # #############################################################################
 
 
-class Geometry_HighLevel:
+class HighLevel:
     """ Benchmark the geometry-oriented routines
 
     In particular:
@@ -29,44 +29,47 @@ class Geometry_HighLevel:
     # Attributes reckognized by asv
 
     # time before benchmark is killed
-    timeout = 60
+    timeout = 30
+    repeat = (1, 10, 20.0)
+    sample_time = 0.100
 
     # -------------------------------------------------------
     # Setup and teardown, run before / after benchmark methods
 
-    def setup(self):
+    def setup_cache(self):
+        """ setup_cache caches setup data and is un only once for all
 
-        # Load a configuration
-        self.conf = tf.load_config('ITER')
+        It should either return data or save a file
+        Data returned is fed to setup(), teardown() and all benchmarks
+
+        """
 
         # prepare input dict for a cam1d
-        self.dcam1d = {
+        dcam1d = {
             'pinhole': [8.38/np.sqrt(2.), 8.38/np.sqrt(2.), 0.],
             'orientation': [-np.pi, 0., 0],
             'focal': 0.08,
             'sensor_nb': 100,
             'sensor_size': 0.3,
-            'config': self.conf,
             'Diag': 'SXR',
             'Exp': 'WEST',
             'Name': 'cam1',
         }
 
         # prepare input dict for a cam2d
-        self.dcam2d = {
+        dcam2d = {
             'pinhole': [8.38, 0., 0.],
             'orientation': [-7*np.pi/8, np.pi/6, 0],
             'focal': 0.08,
             'sensor_nb': 400,
             'sensor_size': 0.2,
-            'config': self.conf,
             'Diag': 'SXR',
             'Exp': 'WEST',
             'Name': 'cam2',
         }
 
         # prepare input dict for particle solid angle toroidal integration
-        self.dpart = {
+        dpart = {
             'part_traj': np.array([
                 [6., 0., 0.], [6., 0.01, -4],
             ]).T,
@@ -75,25 +78,31 @@ class Geometry_HighLevel:
             'DPhi': [-np.pi/2, np.pi/2],
             'vmax': False,
             'approx': False,
+            'plot': False,
         }
+        return dcam1d, dcam2d, dpart
 
-    def teardown(self):
+    def setup(self, out):
+        """ run before each benchmark method, out from setup_cache  """
+        self.conf = tf.load_config('ITER')
+
+    def teardown(self, out):
+        """ run after each benchmark method, out from setup_cache  """
         pass
 
     # -------------------------------------
     # benchmarks methods for geometry tools
 
     # CAMLOS1D
-    def peakmem_camlos1d(self):
-        cam = tf.geom.utils.create_CamLOS1D(**self.dcam1d)
+    def peakmem_camlos1d(self, out):
+        cam = tf.geom.utils.create_CamLOS1D(config=self.conf, **out[0])
 
     # CAMLOS2D
-    def peakmem_camlos2d(self):
-        cam = tf.geom.utils.create_CamLOS2D(**self.dcam2d)
+    def peakmem_camlos2d(self, out):
+        cam = tf.geom.utils.create_CamLOS2D(config=self.conf, **out[1])
 
     # Solid angle toroidal integral for particle
-    def peakmem_solidangle_part(self):
+    def peakmem_solidangle_part(self, out):
         (
-            ptsRZ, sang, indices, reseff, dax,
-        ) = self.conf.calc_solidangle_particle_integrated(**self.dpart)
-        plt.close('all')
+            ptsRZ, sang, indices, reseff,
+        ) = self.conf.calc_solidangle_particle_integrated(**out[2])
