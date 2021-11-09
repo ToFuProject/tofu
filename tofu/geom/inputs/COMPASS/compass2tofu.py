43aa25d129ec55a853e21a1ae2680eed7cd8b39d
Didou09 <didier.vezineT@gmail.com>
2021-11-09 19:28:47 +0100

    [#576] Updated compass2tofu.py and removed useless mat files

--- tofu/geom/inputs/COMPASS/compass2tofu.py
+++ tofu/geom/inputs/COMPASS/compass2tofu.py
@@ -7,9 +7,12 @@ import numpy as np
 import scipy.io as scpio
 
 
+import tofu as tf
+
+
 _PATH_HERE = os.path.dirname(__file__)
 _PATH_INPUTS = os.path.dirname(_PATH_HERE)
-
+_PATH_SAVE = _PATH_INPUTS
 
 
 # #############################################################################
@@ -18,70 +21,86 @@ _PATH_INPUTS = os.path.dirname(_PATH_HERE)
 # #############################################################################
 
 
-def extract(pfe=None):
+def extract(save=False):
 
-    if not pfe.endswith('.mat'):
+    lf = [
+        ff for ff in os.listdir(_PATH_HERE)
+        if ff.endswith('.mat')
+        and 'coordinates' if ff
+        and any([ss in ff for ss in ['vessel', 'limiter']])
+        and ff.startswith('COMPASS')
+    ]
+    if len(lf) == 0:
         return
 
-    out = scpio.loadmat(pfe)
+    # ----------------
+    # Extract all data
 
-    fname = os.path.split(pfe)[1][:-4]
+    dout = {'Ves': {}, 'PFC': {}}
+    for ff in lf:
 
-    dout = None
-    if fname in ['wall_01', 'wall_02', 'wall_03']:
-        R = out['wall'][0, 0][0].ravel()
-        Z = out['wall'][0, 0][1].ravel()
-        vv = int(fname[-2:])
+        pfe = os.path.join(_PATH_HERE, ff)
+        out = scpio.loadmat(pfe)
 
-        poly = np.array(R, Z)
-        cls = 'Ves'
-        name = f'V{vv}'
+        if 'vessel' in ff:
+            kR, kZ = 'R1', 'Z1'
+            name = 'InnerV1'
+        else:
+            kR, kZ = 'R', 'Z'
+            name = 'V0'
 
-    elif all([ss in fname for ss in ['COMPASS', 'coordinates']]):
-        kR, kZ = ('R', 'Z') if 'limiter' in fname else ('R1', 'Z1')
         R = out[kR].ravel()
         Z = out[kZ].ravel()
 
-        if 'vessel' in fname:
-            dout = {
-                'Ves': {
-                    'Name': 'InnerV1',
-                    'poly': np.array(R, Z),
-                },
-            }
+        dout['Ves'][name] = tf.geom.Ves(
+            Poly=np.array([R, Z]),
+            Name=name,
+            Exp='COMPASS',
+            SavePath=_PATH_SAVE,
+        )
 
-        else:
+    # ---------------
+    # Derive PFCs
 
-            # First extract limiter as vessel V0
-            dout = {
-                'Ves': {
-                    'Name': 'V0',
-                    'poly': np.array(R, Z),
-                },
-            }
-
-            # Then extract PFC from limiter / vessel
-            ind = []
-
-
-    elif 'magnetics' in fname:
-        # TBC
-        pass
-
-    elif fname == 'TK3X_D_8871':
-        # TBC
-        pass
-
-    else:
-        msg = "Unknown file"
-        raise Exception(msg)
+    dind = {
+        'lower': {
+            'V0': np.arange(129, 194),
+            'InnerV1': np.arange(9, 20)[::-1],
+        },
+        'upper': {
+            'V0': np.arange(39, 61),
+            'InnerV1': np.arange(36, 46)[::-1],
+        },
+        'inner': {
+            'V0': np.arange(72, 119),
+            'InnerV1': np.r_[4, 3, 2, 1, 0, 51, 50],
+        },
+        'outer': {
+            'V0': np.r_[np.arange(197, 231), np.arange(0, 35)],
+            'InnerV1': np.arange(21, 33)[::-1],
+        },
+    }
+    for k0, v0 in dind.items():
+        poly = np.concatenate(
+            (
+                dout['Ves']['V0'].Poly[:, v0['V0']],
+                dout['Ves']['InnerV1'].Poly[:, v0['InnerV1']],
+            ),
+            axis=1,
+        )
+        dout['PFC'][k0] = tf.geom.PFC(
+            Poly=poly,
+            Name=k0,
+            Exp='COMPASS',
+            SavePath=_PATH_SAVE,
+        )
 
     # ---------------
     # Format output
 
-    if dout is not None:
+    if save:
         for cc in dout.keys():
-            for nn in dout[cc].keys();
-                dout[cc][nn]['Exp'] = 'COMPASS'
+            for nn in dout[cc].keys():
+                dout[cc][nn].save_to_txt(path=None)
 
     return dout
