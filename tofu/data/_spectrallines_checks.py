755ac2095cfa703793c746993865ee3112238ceb
Didou09 <didier.vezineT@gmail.com>
2022-02-07 21:06:54 +0100

    [#604] Moved all elements/ions/charge routines to _spectrallines_checks.py

--- tofu/data/_spectrallines_checks.py
+++ tofu/data/_spectrallines_checks.py
@@ -70,3 +70,174 @@ def int2roman(num):
     return "".join([a for a in roman_num(num)])
 
 
+# #############################################################################
+# #############################################################################
+#               element - ion - charge equivalence
+# #############################################################################
+
+
+def _check_elementioncharge(
+    ION=None, ion=None,
+    element=None, charge=None,
+    warn=None,
+):
+    """ Specific to SpectralLines """
+
+    if warn is None:
+        warn = True
+
+    # Assess if relevant
+    lc = [
+        ION is not None,
+        ion is not None,
+        element is not None and charge is not None,
+    ]
+    if not any(lc):
+        if warn is True:
+            msg = (
+                """
+                To determine ION, ion, element and charge, provide either:
+                - ION:  {}
+                - ion:  {}
+                - element and charge: {}, {}
+                """.format(ION, ion, element, charge)
+            )
+            warnings.warn(msg)
+        return None, None, None, None
+
+    # Get element and charge from ION if any
+    if lc[0] or lc[1]:
+        indc = 1
+        if (lc[0] and ION[1].islower()) or (lc[1] and ion[1].islower()):
+            indc = 2
+
+        # Infer element
+        elementi = ION[:indc] if lc[0] else ion[:indc]
+        if element is not None and element != elementi:
+            msg = (
+                """
+                Inconsistent ION ({}) vs element ({})
+                """.format(element, elementi)
+            )
+            raise Exception(msg)
+
+        # Infer charge
+        if lc[0]:
+            chargei = roman2int(ION[indc:]) - 1
+        else:
+            chargei = int(ion[indc:].replace('+', ''))
+        if charge is not None and charge != chargei:
+            msg = (
+                """
+                Inconsistent ION ({}) vs charge ({})
+                """.format(charge, chargei)
+            )
+            raise Exception(msg)
+        element = elementi
+        charge = chargei
+        if lc[0]:
+            ioni = '{}{}+'.format(element, charge)
+            if lc[1] and ioni != ion:
+                msg = (
+                    """
+                    Inconsistent ION ({}) vs ion ({})
+                    """.format(ION, ion)
+                )
+                raise Exception(msg)
+            ion = ioni
+
+        elif lc[1]:
+            IONi = '{}{}'.format(element, int2roman(charge+1))
+            if lc[0] and IONi != ION:
+                msg = (
+                    """
+                    Inconsistent ion ({}) vs ION ({})
+                    """.format(ion, ION)
+                )
+                raise Exception(msg)
+            ION = IONi
+
+    # ion provided -> element and charge
+    elif lc[2]:
+        ioni = '{}{}+'.format(element, charge)
+        IONi = '{}{}'.format(element, int2roman(charge+1))
+        if ion is not None and ion != ioni:
+            msg = (
+                """
+                Inconsistent (element, charge) ({}, {}) vs ion ({})
+                """.format(element, charge, ion)
+            )
+            raise Exception(msg)
+        if ION is not None and ION != IONi:
+            msg = (
+                """
+                Inconsistent (element, charge) ({}, {}) vs ION ({})
+                """.format(element, charge, ION)
+            )
+            raise Exception(msg)
+        ion = ioni
+        ION = IONi
+
+    return ION, ion, element, charge
+
+
+def _check_elementioncharge_dict(dstatic):
+    """ Specific to SpectralLines """
+
+    # Assess if relevant
+    lk = [kk for kk in ['ion', 'ION'] if kk in dstatic.keys()]
+    if len(lk) == 0:
+        return
+    kion = lk[0]
+    kION = 'ION' if kion == 'ion' else 'ion'
+    if kion == 'ION':
+        dstatic['ion'] = {}
+
+    lerr = []
+    for k0, v0 in dstatic[kion].items():
+        try:
+            if kion == 'ION':
+                ION, ion, element, charge = _check_elementioncharge(
+                    ION=k0,
+                    ion=v0.get('ion'),
+                    element=v0.get('element'),
+                    charge=v0.get('charge'),
+                )
+            else:
+                ION, ion, element, charge = _check_elementioncharge(
+                    ION=v0.get('ION'),
+                    ion=k0,
+                    element=v0.get('element'),
+                    charge=v0.get('charge'),
+                )
+
+            if ION is None:
+                continue
+            if kion == 'ION':
+                dstatic['ion'][ion] = {
+                    'ION': ION,
+                    'element': element,
+                    'charge': charge,
+                }
+            else:
+                dstatic['ion'][k0]['ION'] = ION
+                dstatic['ion'][k0]['element'] = element
+                dstatic['ion'][k0]['charge'] = charge
+
+        except Exception as err:
+            lerr.append((k0, str(err)))
+
+    if kion == 'ION':
+        del dstatic['ION']
+
+    if len(lerr) > 0:
+        lerr = ['\t- {}: {}'.format(pp[0], pp[1]) for pp in lerr]
+        msg = (
+            """
+            The following entries have non-conform ion / ION / element / charge
+            {}
+            """.format('\n'.join(lerr))
+        )
+        raise Exception(msg)
+
+
