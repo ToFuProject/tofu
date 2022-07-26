2527c877c8009e552dd2156593031f7704f660ae
OBREJAN Kévin 262976 <KO262976@canismajor.intra.cea.fr>
2022-07-26 15:35:35 +0200

    debug CI

--- tofu/tests/tests01_geom/test_01_GG.py
+++ tofu/tests/tests01_geom/test_01_GG.py
@@ -571,6 +571,7 @@ def test12_Ves_Smesh_Lin(VPoly=VPoly):
         # assert np.allclose(dLr, dLri)
         # assert np.allclose(Rref,Rrefi)
         assert all([dXr == dXri, dY0r == dY0ri, dZ0r == dZ0ri])
+
     print(egewgwrgwr)
 
 
