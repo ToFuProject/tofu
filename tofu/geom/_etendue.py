43d35d29fe9dffcd3998dbd14e4f52aee1235b42
dvezinet <didier.vezinet@gmail.com>
2022-07-28 12:42:53 +0200

    [#658] compute_etendue() running but strange behaviour for tilted detector, TODO: understand / debug + unit tests

--- tofu/geom/_etendue.py
+++ tofu/geom/_etendue.py
@@ -25,7 +25,9 @@ def compute_etendue(
     aperture=None,
     analytical=None,
     numerical=None,
+    check=None,
     res=None,
+    margin=None,
     verb=None,
     plot=None,
 ):
@@ -42,13 +44,16 @@ def compute_etendue(
     (
         det, aperture,
         analytical, numerical,
-        res, verb, plot,
+        res, margin,
+        check, verb, plot,
     ) = _compute_etendue_check(
         det=det,
         aperture=aperture,
         analytical=analytical,
         numerical=numerical,
         res=res,
+        margin=margin,
+        check=check,
         verb=verb,
         plot=plot,
     )
@@ -101,6 +106,8 @@ def compute_etendue(
             los_x=los_x,
             los_y=los_y,
             los_z=los_z,
+            margin=margin,
+            check=check,
             verb=verb,
         )
 
@@ -114,6 +121,7 @@ def compute_etendue(
         dax = _plot_etendues(
             etend0=etend0,
             etend1=etend1,
+            res=res,
         )
 
     # --------------------
@@ -141,6 +149,8 @@ def _compute_etendue_check(
     analytical=None,
     numerical=None,
     res=None,
+    margin=None,
+    check=None,
     verb=None,
     plot=None,
 ):
@@ -358,6 +368,24 @@ def _compute_etendue_check(
         res = np.atleast_1d(res).ravel()
 
     # -----------
+    # margin
+
+    margin = ds._generic_check._check_var(
+        margin, 'margin',
+        types=float,
+        default=0.05,
+    )
+
+    # -----------
+    # check
+
+    check = ds._generic_check._check_var(
+        check, 'check',
+        types=bool,
+        default=True,
+    )
+
+    # -----------
     # verb
 
     verb = ds._generic_check._check_var(
@@ -375,7 +403,7 @@ def _compute_etendue_check(
         msg = "Arg plot must be a bool"
         raise Exception(msg)
 
-    return det, aperture, analytical, numerical, res, verb, plot
+    return det, aperture, analytical, numerical, res, margin, check, verb, plot
 
 
 # #############################################################################
@@ -476,7 +504,7 @@ def _compute_etendue_prepare(
         res = min(
             np.sqrt(det_surface),
             np.sqrt(np.min(np.diff(ap_out_x0)**2 + np.diff(ap_out_x1)**2))
-        ) * np.r_[0.25, 0.1, 0.05, 0.025]
+        ) * np.r_[2., 1., 0.5, 0.2]
 
     return (
         det_surface, ap_surface, distances,
@@ -495,15 +523,13 @@ def _compute_etendue_numerical(
     det=None,
     aperture=None,
     res=None,
-    k_los=None,
+    margin=None,
     los_x=None,
     los_y=None,
     los_z=None,
+    check=None,
     verb=None,
 ):
-
-    if k_los is None:
-        k_los = 1.2
 
     shape0 = det['cents_x'].shape
     cents_x = det['cents_x'].ravel()
@@ -545,33 +571,40 @@ def _compute_etendue_numerical(
         deti['e1_y'] = np.r_[e1_y[ii]]
         deti['e1_z'] = np.r_[e1_z[ii]]
 
-        # get det centers to aperture corners vectors
-        PA_x = aperture['poly_x'] - cents_x[ii]
-        PA_y = aperture['poly_y'] - cents_y[ii]
-        PA_z = aperture['poly_z'] - cents_z[ii]
+        # get det corners to aperture corners vectors
+        out_c_x0 = np.r_[0, deti['outline_x0']]
+        out_c_x1 = np.r_[0, deti['outline_x1']]
+
+        det_Px = cents_x[ii] + out_c_x0*e0_x[ii] + out_c_x1*e1_x[ii]
+        det_Py = cents_y[ii] + out_c_x0*e0_y[ii] + out_c_x1*e1_y[ii]
+        det_Pz = cents_z[ii] + out_c_x0*e0_z[ii] + out_c_x1*e1_z[ii]
+
+        PA_x = aperture['poly_x'][:, None] - det_Px[None, :]
+        PA_y = aperture['poly_y'][:, None] - det_Py[None, :]
+        PA_z = aperture['poly_z'][:, None] - det_Pz[None, :]
 
         # get length along los
-        k_losi = (
-            k_los
+        k_los = (
+            (1. + margin)
             * np.max(PA_x * los_x[ii] + PA_y * los_y[ii] + PA_z * los_z[ii])
         )
 
         # get center of plane perpendicular to los
-        c_los_x = cents_x[ii] + k_losi * los_x[ii]
-        c_los_y = cents_y[ii] + k_losi * los_y[ii]
-        c_los_z = cents_z[ii] + k_losi * los_z[ii]
+        c_los_x = cents_x[ii] + k_los * los_x[ii]
+        c_los_y = cents_y[ii] + k_los * los_y[ii]
+        c_los_z = cents_z[ii] + k_los * los_z[ii]
 
         # get projections of corners on plane perp. to los
         sca0 = (
-            (c_los_x - cents_x[ii]) * los_x[ii]
-            + (c_los_y - cents_y[ii]) * los_y[ii]
-            + (c_los_z - cents_z[ii]) * los_z[ii]
+            (c_los_x - det_Px[None, :]) * los_x[ii]
+            + (c_los_y - det_Py[None, :]) * los_y[ii]
+            + (c_los_z - det_Pz[None, :]) * los_z[ii]
         )
         sca1 = PA_x * los_x[ii] + PA_y * los_y[ii] + PA_z * los_z[ii]
 
         k_plane = sca0 / sca1
 
-        # get projections on det_e0 and det_e1 in plane
+        # get LOS-specific unit vectors
 
         e0_xi = los_y[ii] * e1_z[ii] - los_z[ii] * e1_y[ii]
         e0_yi = los_z[ii] * e1_x[ii] - los_x[ii] * e1_z[ii]
@@ -585,15 +618,17 @@ def _compute_etendue_numerical(
         e1_yi = los_z[ii] * e0_xi - los_x[ii] * e0_zi
         e1_zi = los_x[ii] * e0_yi - los_y[ii] * e0_xi
 
+        # get projections on det_e0 and det_e1 in plane
+
         x0 = (
-            ((cents_x[ii] + k_plane * PA_x) - c_los_x)*e0_xi
-            + ((cents_y[ii] + k_plane * PA_y) - c_los_y)*e0_yi
-            + ((cents_z[ii] + k_plane * PA_z) - c_los_z)*e0_zi
+            ((det_Px[None, :] + k_plane * PA_x) - c_los_x)*e0_xi
+            + ((det_Py[None, :] + k_plane * PA_y) - c_los_y)*e0_yi
+            + ((det_Pz[None, :] + k_plane * PA_z) - c_los_z)*e0_zi
         )
         x1 = (
-            ((cents_x[ii] + k_plane * PA_x) - c_los_x)*e1_xi
-            + ((cents_y[ii] + k_plane * PA_y) - c_los_y)*e1_yi
-            + ((cents_z[ii] + k_plane * PA_z) - c_los_z)*e1_zi
+            ((det_Px[None, :] + k_plane * PA_x) - c_los_x)*e1_xi
+            + ((det_Py[None, :] + k_plane * PA_y) - c_los_y)*e1_yi
+            + ((det_Pz[None, :] + k_plane * PA_z) - c_los_z)*e1_zi
         )
 
         x0_min, x0_max = np.min(x0), np.max(x0)
@@ -606,20 +641,16 @@ def _compute_etendue_numerical(
 
         for jj in range(res.size):
 
-            if verb is True:
-                msg = f"\tres = {res[jj]} ({jj+1} / {res.size})"
-                print(msg)
+            n0 = int(np.ceil((1. + 2.*margin)*w0 / res[jj]))
+            n1 = int(np.ceil((1. + 2.*margin)*w1 / res[jj]))
 
-            n0 = int(np.ceil(w0 / res[jj]))
-            n1 = int(np.ceil(w1 / res[jj]))
-
-            d0 = w0 / n0
-            d1 = w1 / n1
+            d0 = (1. + 2.*margin)*w0 / n0
+            d1 = (1. + 2.*margin)*w1 / n1
 
             ds = d0 * d1
 
-            pts_0 = np.linspace(x0_min, x0_max, n0 + 1)
-            pts_1 = np.linspace(x1_min, x1_max, n1 + 1)
+            pts_0 = np.linspace(x0_min - margin*w0, x0_max + margin*w0, n0 + 1)
+            pts_1 = np.linspace(x1_min - margin*w1, x1_max + margin*w1, n1 + 1)
             pts_0 = 0.5 * (pts_0[1:] + pts_0[:-1])
             pts_1 = 0.5 * (pts_1[1:] + pts_1[:-1])
 
@@ -633,34 +664,93 @@ def _compute_etendue_numerical(
                 c_los_z + pts_0[:, None] * e0_z + pts_1[None, :] * e1_z
             ).ravel()
 
-            # debug
-            # pts_x = np.r_[c_los_x]
-            # pts_y = np.r_[c_los_y]
-            # pts_z = np.r_[c_los_z]
+            if verb is True:
+                msg = (
+                    f"\tres = {res[jj]} ({jj+1} / {res.size})"
+                    f"    nb. of points = {pts_x.size}"
+                )
+                print(msg)
 
             # ----------------------------------
             # compute solid angle for each pixel
 
-            solid_angle = _comp_solidangles.calc_solidangle_apertures(
-                # observation points
-                pts_x=pts_x,
-                pts_y=pts_y,
-                pts_z=pts_z,
-                # polygons
-                apertures=aperture,
-                detectors=deti,
-                # possible obstacles
-                config=None,
-                # parameters
-                visibility=False,
-                return_vector=False,
-                return_flat_pts=True,
-                return_flat_det=True,
-            )
+            if check is True:
+                solid_angle = _comp_solidangles.calc_solidangle_apertures(
+                    # observation points
+                    pts_x=pts_x,
+                    pts_y=pts_y,
+                    pts_z=pts_z,
+                    # polygons
+                    apertures=aperture,
+                    detectors=deti,
+                    # possible obstacles
+                    config=None,
+                    # parameters
+                    visibility=False,
+                    return_vector=False,
+                    return_flat_pts=True,
+                    return_flat_det=True,
+                )
 
-            etendue[jj, ii] = np.sum(solid_angle) * ds
+                sar = solid_angle.reshape((n0, n1))
+                c0 = (
+                    np.allclose(sar[0, :], 0)
+                    and np.allclose(sar[-1, :], 0)
+                    and np.allclose(sar[:, 0], 0)
+                    and np.allclose(sar[:, -1], 0)
+                )
+                if not c0:
+                    # debug
+                    plt.figure()
+                    plt.imshow(
+                        sar.T,
+                        extent=(
+                            x0_min - margin*w0, x0_max + margin*w0,
+                            x1_min - margin*w1, x1_max + margin*w1,
+                        ),
+                        interpolation='nearest',
+                        origin='lower',
+                        aspect='equal',
+                    )
+                    plt.plot(
+                        x0.ravel(), x1.ravel(), c='r', marker='o', ls='None',
+                    )
+                    plt.plot(
+                        pts_0, np.mean(pts_1)*np.ones((n0,)),
+                        c='k', marker='.', ls='None',
+                    )
+                    plt.plot(
+                       np.mean(pts_0)*np.ones((n1,)), pts_1,
+                        c='k', marker='.', ls='None',
+                    )
+                    plt.gca().set_xlabel('x0')
+                    plt.gca().set_xlabel('x1')
+                    import pdb; pdb.set_trace()
+                    msg = "Something is wrong with solid_angle or sampling"
+                    raise Exception(msg)
+                else:
+                    etendue[jj, ii] = np.sum(solid_angle) * ds
 
-            import pdb; pdb.set_trace()     # DB
+            else:
+                etendue[jj, ii] = np.sum(
+                    _comp_solidangles.calc_solidangle_apertures(
+                        # observation points
+                        pts_x=pts_x,
+                        pts_y=pts_y,
+                        pts_z=pts_z,
+                        # polygons
+                        apertures=aperture,
+                        detectors=deti,
+                        # possible obstacles
+                        config=None,
+                        # parameters
+                        visibility=False,
+                        return_vector=False,
+                        return_flat_pts=True,
+                        return_flat_det=True,
+                    )
+                ) * ds
+
 
     # --------------
     # reshape output
@@ -680,14 +770,34 @@ def _compute_etendue_numerical(
 def _plot_etendues(
     etend0=None,
     etend1=None,
+    res=None,
 ):
 
     # -------------
     # prepare data
 
-    if etend0.ndim > 2:
-        etend0 = etend0.reshape((etend0.shape[0], -1))
-        etend1 = etend1.reshape((etend0.shape[0], -1))
+    nmax = 0
+    if etend0 is not None:
+        if etend0.ndim > 2:
+            etend0 = etend0.reshape((etend0.shape[0], -1))
+        nmax = max(nmax, etend0.shape[0])
+    if etend1 is not None:
+        if etend1.ndim > 2:
+            etend1 = etend1.reshape((etend1.shape[0], -1))
+        nmax = max(nmax, etend1.shape[0])
+
+    x0 = None
+    if etend0 is not None:
+        x0 = [
+            f'order {ii}' if ii < 3 else '' for ii in range(nmax)
+        ]
+    if etend1 is not None:
+        x1 = [f'{res[ii]}' if ii < res.size-1 else '' for ii in range(nmax)]
+        if x0 is None:
+            x0 = x1
+        else:
+            x0 = [f'{x0[ii]}\n{x1[ii]}' for ii in range(nmax)]
+
 
     # -------------
     # prepare axes
@@ -698,8 +808,8 @@ def _plot_etendues(
     ax.set_ylabel('Etendue '+ r'($m^2.sr$)', size=12, fontweight='bold')
     ax.set_xlabel('order of approximation', size=12, fontweight='bold')
 
-    ax.set_xticks(range(1, etend0.shape[0] + 1))
-    ax.set_xticklabels(['0th', '1st', '2nd'])
+    ax.set_xticks(range(0, nmax))
+    ax.set_xticklabels(x0)
 
     # -------------
     # plot
