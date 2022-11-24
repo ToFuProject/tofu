3b17679942e3246040a4dc17fed1b135a6dc21ae
dvezinet <didier.vezinet@gmail.com>
2022-11-24 20:07:47 +0100

    [#704] Advancing compute_diagnostic_signal()

--- tofu/data/_class8_compute_signal.py
+++ tofu/data/_class8_compute_signal.py
@@ -14,16 +14,16 @@ import datastock as ds
 
 def compute_signal(
     coll=None,
-    key=None,
+    key_diag=None,
     key_cam=None,
+    # to be integrated
+    key_emiss=None,
     # sampling
     method=None,
     res=None,
     mode=None,
     # signal
     brightness=None,
-    # to be integrated
-    key_emiss=None,
     # store
     store=None,
     key_signal=None,
@@ -36,14 +36,14 @@ def compute_signal(
     # --------------
 
     (
-        key, key_cam, spectro, is2d,
+        key_diag, key_cam, spectro, is2d,
         method, brightness,
-        key_emiss,
+        key_emiss, key_mesh0,
         store, key_signal,
         returnas,
     ) = _compute_signal_check(
         coll=coll,
-        key=key,
+        key_diag=key_diag,
         key_cam=key_cam,
         # sampling
         method=method,
@@ -61,15 +61,28 @@ def compute_signal(
     )
 
     # -------------
+    # prepare 
+    # --------------
+
+    shape_emiss = coll.ddata[key_emiss]['shape']
+
+    key_kR = coll.dobj['mesh'][key_mesh0]['knots'][0]
+    radius_max = np.max(coll.ddata[key_kR]['data'])
+
+    # -------------
     # compute 
     # --------------
 
-    if method == 'LOS':
+    if method == 'los':
         dout = _compute_los(
+            coll=coll,
+            is2d=is2d,
+            key_diag=key_diag,
             key_cam=key_cam,
             res=res,
             mode=mode,
             key_emiss=key_emiss,
+            radius_max=radius_max,
         )
 
     else:
@@ -87,7 +100,7 @@ def compute_signal(
 
 def _compute_signal_check(
     coll=None,
-    key=None,
+    key_diag=None,
     key_cam=None,
     # sampling
     method=None,
@@ -105,15 +118,15 @@ def _compute_signal_check(
 ):
 
     # key, key_cam
-    key, key_cam = coll.get_diagnostic_cam(key=key, key_cam=key_cam)
-    spectro = coll.dobj['diagnostic'][key]['spectro']
-    is2d = coll.dobj['diagnostic'][key]['is2d']
+    key_diag, key_cam = coll.get_diagnostic_cam(key=key_diag, key_cam=key_cam)
+    spectro = coll.dobj['diagnostic'][key_diag]['spectro']
+    is2d = coll.dobj['diagnostic'][key_diag]['is2d']
 
     # method
     method = ds._generic_check._check_var(
         method, 'method',
         types=str,
-        defaut='los',
+        default='los',
         allowed=['los', 'vos'],
     )
 
@@ -135,6 +148,15 @@ def _compute_signal_check(
         allowed=lok,
     )
 
+    # key_mesh0
+    key_bs = coll.ddata[key_emiss]['bsplines']
+    key_mesh = coll.dobj['bsplines'][key_bs]['mesh']
+    mtype = coll.dobj['mesh'][key_mesh]['type']
+    if mtype == 'polar':
+        key_mesh0 = coll.dobj['mesh'][key_mesh]['submesh']
+    else:
+        key_mesh0 = key_mesh
+
     # store
     store = ds._generic_check._check_var(
         store, 'store',
@@ -146,7 +168,7 @@ def _compute_signal_check(
     key_signal = ds._generic_check._check_var(
         key_signal, 'key_signal',
         types=str,
-        default=f'{key}_synth',
+        default=f'{key_diag}_synth',
         excluded=list(coll.ddata.keys()),
     )
 
@@ -158,9 +180,9 @@ def _compute_signal_check(
     )
 
     return (
-        key, key_cam, spectro, is2d,
+        key_diag, key_cam, spectro, is2d,
         method, brightness,
-        key_emiss,
+        key_emiss, key_mesh0,
         store, key_signal,
         returnas,
     )
@@ -174,17 +196,65 @@ def _compute_signal_check(
 
 def _compute_los(
     coll=None,
+    is2d=None,
+    key_diag=None,
     key_cam=None,
     res=None,
     mode=None,
     key_emiss=None,
+    radius_max=None,
 ):
 
     # loop on cameras
     dout = {}
+    doptics = coll.dobj['diagnostic'][key_diag]['doptics']
     for k0 in key_cam:
 
-        data = 
+        npix = coll.dobj['camera'][k0]['dgeom']['pix_nb']
+        key_los = doptics[k0]['los']
+
+        if is2d:
+            pass
+        else:
+            ptsx, ptsy, ptsz = coll.sample_rays(
+                key=key_los,
+                res=res,
+                mode=mode,
+                segment=None,
+                radius_max=radius_max,
+                concatenate=True,
+                return_coords=None,
+            )
+
+            inan = np.isnan(ptsx)
+            iok = ~inan
+            R = np.hypot(ptsx, ptsy)
+
+            data, units = coll.interpolate_profile2d(
+                key=key_emiss,
+                coefs=None,
+                R=R[iok],
+                Z=ptsz[iok],
+                grid=False,
+                radius_vs_time=None,
+                azone=None,
+                t=None,
+                indt=None,
+                indt_strict=None,
+                indbs=None,
+                details=False,
+                reshape=None,
+                res=None,
+                crop=None,
+                nan0=None,
+                nan_out=None,
+                imshow=None,
+                return_params=None,
+                store=False,
+                inplace=None,
+            )
+
+            import pdb; pdb.set_trace()     # DB
 
         dout[k0] = {
             'data': data,
