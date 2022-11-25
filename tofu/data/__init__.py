4b029e06ad31059e39f48964489f463671c64ac2
dvezinet <didier.vezinet@gmail.com>
2022-11-25 19:38:08 +0100

    [#704] Renamed default tofu class to Collection

--- tofu/data/__init__.py
+++ tofu/data/__init__.py
@@ -6,5 +6,5 @@ from ._core import *
 from ._DataCollection_class1_interactivity import *
 from ._spectrallines_class import *
 from ._class1_Plasma2D import *
-from ._class8_Diagnostic import *
+from ._class9_GeometryMatrix import GeometryMatrix as Collection
 from ._inversions_checks import get_available_inversions_algo
