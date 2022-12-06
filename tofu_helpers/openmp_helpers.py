4a2058e51f61b179065e9fe26d9a22119d0af6f3
OBREJAN Kévin 262976 <KO262976@canismajor.intra.cea.fr>
2022-12-06 13:31:59 +0100

    Undid recent changes

--- tofu_helpers/openmp_helpers.py
+++ tofu_helpers/openmp_helpers.py
@@ -78,20 +78,6 @@ def get_openmp_flag(compiler):
     else:
         compiler = compiler.__class__.__name__
 
-
-    print(
-        f"""
-
-        Compiler: {compiler}
-
-        sys.platform: {sys.platform}
-
-        os.getenv('CPPFLAGS', ''): {os.getenv('CPPFLAGS', '')}
-
-
-        """
-    )
-
     if sys.platform == "win32" and ('icc' in compiler or 'icl' in compiler):
         return ['/Qopenmp']
     elif sys.platform == "win32":
@@ -100,8 +86,6 @@ def get_openmp_flag(compiler):
         return ['-qopenmp']
     elif sys.platform == "darwin" and 'openmp' in os.getenv('CPPFLAGS', ''):
         return ['-openmp']
-    # elif sys.platform == "darwin" and 'gcc' in compiler:
-        # return ['-fopenmp=libomp']      # DB
     # Default flag for GCC and clang:
     return ['-fopenmp']
 
@@ -148,19 +132,6 @@ def check_for_openmp():
                 capture_output=True
             )
             print(f"subprocess.call(..) -> {result}")
-            result2 = subprocess.run(
-                [compiler, '--version'],
-                shell=is_platform_windows(),
-                capture_output=True
-            )
-            print(f"subprocess.call(..) -> {result2}")
-            result2 = subprocess.run(
-                [compiler, '-v'],
-                shell=is_platform_windows(),
-                capture_output=True
-            )
-            print(f"subprocess.call(..) -> {result2}")
-
     except subprocess.CalledProcessError as err:
         result = err    # -1
         print("except")
