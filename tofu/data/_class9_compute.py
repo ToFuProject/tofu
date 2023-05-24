9b39e66a00a2d79b69ccb710f841d24057226581
dvezinet <didier.vezinet@gmail.com>
2023-05-24 12:01:02 +0200

    [#767] add_geometry_matrix(method='vos', dvos=dvos) implemented for non-spectro

--- tofu/data/_class9_compute.py
+++ tofu/data/_class9_compute.py
@@ -83,10 +83,6 @@ def compute(
     # prepare
     # -----------
 
-    wm = coll._which_mesh
-    wbs = coll._which_bsplines
-    shapebs = coll.dobj[wbs][key_bs]['shape']
-
     # prepare indices
     indbs = coll.select_ind(
         key=key_bs,
@@ -132,7 +128,27 @@ def compute(
         )
 
     else:
-        raise NotImplementedError()
+        dout, axis = _compute_vos(
+            coll=coll,
+            key=key,
+            key_bs=key_bs,
+            key_diag=key_diag,
+            key_cam=key_cam,
+            # dvos
+            dvos=dvos,
+            # sampling
+            indbs=indbs,
+            res=res,
+            mode=mode,
+            radius_max=radius_max,
+            # groupby=groupby,
+            shape_mat=shape_mat,
+            sli_mat=sli_mat,
+            axis_pix=axis_pix,
+            # other
+            brightness=brightness,
+            verb=verb,
+        )
 
     # ---------------
     # check
@@ -293,7 +309,7 @@ def _compute_check(
 
     # dvos
     if method == 'vos':
-        dvos = coll.check_diagnostic_vos(
+        dvos = coll.check_diagnostic_dvos(
             key=key_diag,
             key_cam=key_cam,
             dvos=dvos,
@@ -329,6 +345,7 @@ def _compute_check(
         subkey, key_bs0, key_m0,
         key_diag, key_cam,
         radius_max, method, res, mode, crop,
+        dvos,
         brightness,
         store, verb,
     )
@@ -554,21 +571,186 @@ def _compute_los(
 
 def _compute_vos(
     coll=None,
-    is2d=None,
+    key=None,
+    key_bs=None,
     key_diag=None,
     key_cam=None,
+    # dvos
+    dvos=None,
+    # sampling
+    indbs=None,
     res=None,
     mode=None,
     key_integrand=None,
     radius_max=None,
-    groupby=None,
-    val_init=None,
+    is3d=None,
+    # slicing
+    shape_mat=None,
+    sli_mat=None,
+    axis_pix=None,
+    # parameters
     brightness=None,
+    verb=None,
 ):
 
+    # -----
+    # units
 
+    units = asunits.m
+    units_coefs = asunits.Unit()
 
-    return None, None
+    # -------------
+    # mesh sampling
+
+    wbs = coll._which_bsplines
+    key_mesh = coll.dobj[wbs][key_bs]['mesh']
+
+    # res
+    lres = set([tuple(v0['res_RZ']) for v0 in dvos.values()])
+    if len(lres) > 1:
+        msg = "All cameras do not have the same mesh sampling resolution"
+        raise Exception(msg)
+
+    res = list(list(lres)[0])
+
+    # mesh sampling
+    dsamp = coll.get_sample_mesh(
+        key=key_mesh,
+        res=res,
+        mode='abs',
+        grid=False,
+        in_mesh=True,
+        # non-used
+        x0=None,
+        x1=None,
+        Dx0=None,
+        Dx1=None,
+        imshow=False,
+        store=False,
+        kx0=None,
+        kx1=None,
+    )
+
+    x0u = dsamp['x0']['data']
+    x1u = dsamp['x1']['data']
+
+    # ----------------
+    # loop on cameras
+
+    dout = {}
+    doptics = coll.dobj['diagnostic'][key_diag]['doptics']
+    for k0 in key_cam:
+
+        npix = coll.dobj['camera'][k0]['dgeom']['pix_nb']
+        key_mat = f'{key}_{k0}'
+
+        # -------------
+        # slicing
+
+        is2d = coll.dobj['camera'][k0]['dgeom']['nd'] == '2d'
+        if is2d:
+            n0, n1 = coll.dobj['camera'][k0]['dgeom']['shape']
+            sli = lambda ii: (ii // n1, ii % n1, slice(None))
+        else:
+            sli = lambda ii: (ii, slice(None))
+
+        # shape, key
+        sh = tuple([npix if ss is None else ss for ss in shape_mat])
+        mat = np.zeros(sh, dtype=float)
+
+        # ---------------------------------------------------
+        # loop on group of pixels (to limit memory footprint)
+
+        anyok = False
+        for ii in range(npix):
+
+            # verb
+            if verb is True:
+                msg = f"\t- '{key_mat}': pixel {ii + 1} / {npix}"
+                msg += f"\t{(mat > 0).sum()} / {mat.size}"
+                end = '\n' if ii == npix - 1 else '\r'
+                print(msg, flush=True, end=end)
+
+            # sample los
+            indok = np.isfinite(dvos[k0]['sang']['data'][sli(ii)])
+            if not np.any(indok):
+                continue
+
+            # indices + dv
+            indr = dvos[k0]['indr'][sli(ii)][indok]
+            indz = dvos[k0]['indz'][sli(ii)][indok]
+
+            # -------------
+            # interpolate
+
+            # datai, units, refi = coll.interpolate(
+            douti = coll.interpolate(
+                keys=None,
+                ref_key=key_bs,
+                x0=x0u[indr],
+                x1=x1u[indz],
+                submesh=True,
+                grid=False,
+                # azone=None,
+                indbs_tf=indbs,
+                details=True,
+                crop=None,
+                nan0=True,
+                val_out=np.nan,
+                return_params=False,
+                store=False,
+            )[f'{key_bs}_details']
+
+            datai, refi = douti['data'], douti['ref']
+            axis = refi.index(None)
+            iok = np.isfinite(datai)
+
+            if not np.any(iok):
+                continue
+
+            datai[~iok] = 0.
+
+            # ------------
+            # integrate
+
+            # check and update slice
+            assert datai.ndim in [2, 3], datai.shape
+            sli_mat[axis_pix] = ii
+
+            # integrate
+            mat[tuple(sli_mat)] = np.sum(datai, axis=axis)
+
+            anyok = True
+
+        # --------------
+        # post-treatment
+
+        if anyok:
+            # brightness
+            if brightness is True:
+                ketend = doptics[k0]['etendue']
+                units_coefs = coll.ddata[ketend]['units']
+                etend = coll.ddata[ketend]['data']
+                sh_etend = [-1 if aa == axis else 1 for aa in range(len(refi))]
+                mat /= etend.reshape(sh_etend)
+
+            # set ref
+            refi = list(refi)
+            refi[axis] = coll.dobj['camera'][k0]['dgeom']['ref_flat']
+            refi = tuple(np.r_[refi[:axis], refi[axis], refi[axis+1:]])
+
+        else:
+            refi = None
+            axis = None
+
+        # fill dout
+        dout[key_mat] = {
+            'data': mat,
+            'ref': refi,
+            'units': units * units_coefs,
+        }
+
+    return dout, axis
 
 
 # ###################
