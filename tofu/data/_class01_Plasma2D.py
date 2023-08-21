3e79c4ac46940de2f232cb114348d5686c887bd6
Didier Vezinet <dvezinet@cfs.energy>
2023-08-21 14:21:29 -0400

    [#820] Propagated uniform=bool to plot_as_profile2d()

--- tofu/data/_class01_Plasma2D.py
+++ tofu/data/_class01_Plasma2D.py
@@ -166,6 +166,7 @@ class Plasma2D(Previous):
         dref_vectorZ=None,
         dref_vectorU=None,
         ref_vector_strategy=None,
+        uniform=None,
         # interpolation
         val_out=None,
         nan0=None,
@@ -198,6 +199,7 @@ class Plasma2D(Previous):
             dref_vectorZ=dref_vectorZ,
             dref_vectorU=dref_vectorU,
             ref_vector_strategy=ref_vector_strategy,
+            uniform=uniform,
             # interpolation
             val_out=val_out,
             nan0=nan0,
