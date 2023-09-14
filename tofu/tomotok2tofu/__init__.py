940154642357ac118dd825f677be8924a70a4530
dvezinet <didier.vezinet@gmail.com>
2023-09-14 03:33:01 +0200

    [#829] Fixed tomotok robustness

--- tofu/tomotok2tofu/__init__.py
+++ tofu/tomotok2tofu/__init__.py
@@ -16,6 +16,8 @@ try:
         from tofu.tomotok2tofu._core import *
     except Exception:
         from ._core import *
+    import tomotok.core as tmtkc
+    assert hasattr(tmtkc, 'inversions')
 except Exception as err:
     msg = str(traceback.format_exc())
     msg += "\n\n    => optional sub-package tofu.tomotok2tofu not usable\n"
