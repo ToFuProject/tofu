e5312ecc3fbc1b85ea523b1d954fdf6dd4137aef
Didier Vezinet <dvezinet@cfs.energy>
2024-06-12 18:40:52 -0400

    [#942] Minor cleanup of _class02_tangency_radius.py

--- tofu/data/_class02_tangency_radius.py
+++ tofu/data/_class02_tangency_radius.py
@@ -13,147 +13,10 @@ import datastock as ds
 from . import _class2_check as _check
 
 
-
 # ###############################################################
 # ###############################################################
 #                  tangency radius
 # ###############################################################
-
-
-def _tangency_radius_check(
-    coll=None,
-    key=None,
-    key_cam=None,
-    quantity=None,
-    segment=None,
-    lim_to_segments=None,
-    axis_pt=None,
-    axis_vect=None,
-):
-
-    # key
-    key = _check._check_key(coll=coll, key=key, key_cam=key_cam)
-
-    # quantity
-    quantity = ds._generic_check._check_var(
-        quantity, 'quantity',
-        types=str,
-        default='tangency_radius',
-        allowed=['alpha', 'tangency_radius', 'length'],
-    )
-
-    # lim_to_segments
-    lim_to_segments = ds._generic_check._check_var(
-        lim_to_segments, 'lim_to_segments',
-        types=bool,
-        default=False,
-    )
-
-    # segment
-    if segment is not None:
-        segment = np.atleast_1d(segment).astype(int).ravel()
-
-    # --------
-    # tangency radius-specific
-
-    if quantity == 'tangency_radius':
-        # axis_pt
-        if axis_pt is None:
-            axis_pt = [0., 0., 0.]
-
-        axis_pt = ds._generic_check._check_flat1darray(
-            axis_pt, 'axis_pt',
-            dtype=float,
-            size=3,
-            can_be_None=False,
-        )
-
-        # axis_vect
-        if axis_vect is None:
-            axis_vect = [0., 0., 1.]
-
-        axis_vect = ds._generic_check._check_flat1darray(
-            axis_vect, 'axis_vect',
-            dtype=float,
-            size=3,
-            norm=True,
-            can_be_None=False,
-        )
-
-    return key, quantity, segment, lim_to_segments, axis_pt, axis_vect
-
-
-def _tangency_radius_prepare(
-    coll=None,
-    key=None,
-    quantity=None,
-    segment=None,
-    axis_pt=None,
-    axis_vect=None,
-):
-
-    pts_x, pts_y, pts_z = coll.get_rays_pts(key=key)
-    i0 = np.arange(0, pts_x.shape[0])
-
-    # select segment
-    if segment is not None:
-        npts = pts_x.shape[0]
-        segment[segment < 0] = npts - 1 + segment[segment < 0]
-
-        iseg = np.r_[segment, segment[-1] + 1]
-        i0 = i0[iseg]
-        pts_x = pts_x[iseg, :]
-        pts_y = pts_y[iseg, :]
-        pts_z = pts_z[iseg, :]
-
-
-    # define vectors
-    ABx = np.diff(pts_x, axis=0)
-    ABy = np.diff(pts_y, axis=0)
-    ABz = np.diff(pts_z, axis=0)
-
-    if quantity == 'tangency_radius':
-        AOx = axis_pt[0] - pts_x[:-1, ...]
-        AOy = axis_pt[1] - pts_y[:-1, ...]
-        AOz = axis_pt[2] - pts_z[:-1, ...]
-
-        # --------
-        # get kk
-
-        # Eq:
-        # k^2 |AB x v|^2 + 2k((AB.v)(AO.v) - AB.AO) + |AO x v|^2 = dist^2
-        #
-        # minimum:
-        # k |AB x v|^2 + (AB.v)(AO.v) - AB.AO = 0
-
-        ABvn2 = (
-            (ABy*axis_vect[2] - ABz*axis_vect[1])**2
-            + (ABz*axis_vect[0] - ABx*axis_vect[2])**2
-            + (ABx*axis_vect[1] - ABy*axis_vect[0])**2
-            )
-        AOvn2 = (
-            (AOy*axis_vect[2] - AOz*axis_vect[1])**2
-            + (AOz*axis_vect[0] - AOx*axis_vect[2])**2
-            + (AOx*axis_vect[1] - AOy*axis_vect[0])**2
-            )
-
-        ABv = ABx*axis_vect[0] + ABy*axis_vect[1] + ABz*axis_vect[2]
-        AOv = AOx*axis_vect[0] + AOy*axis_vect[1] + AOz*axis_vect[2]
-        ABAO = ABx*AOx + ABy*AOy + ABz*AOz
-        B = ABv*AOv - ABAO
-
-    else:
-        AOx, AOy, AOz = None, None, None
-        ABvn2, AOvn2, B = None, None, None
-        i0 = None
-
-    return (
-        pts_x, pts_y, pts_z,
-        ABx, ABy, ABz,
-        AOx, AOy, AOz,
-        ABvn2, AOvn2, B,
-        i0,
-    )
 
 
 def _tangency_radius(
@@ -254,6 +117,158 @@ def _tangency_radius(
     # return
 
     return out, kk, ref
+
+
+# ###############################################################
+#              tangency radius - check
+# ###############################################################
+
+
+def _tangency_radius_check(
+    coll=None,
+    key=None,
+    key_cam=None,
+    quantity=None,
+    segment=None,
+    lim_to_segments=None,
+    axis_pt=None,
+    axis_vect=None,
+):
+
+    # key
+    key = _check._check_key(coll=coll, key=key, key_cam=key_cam)
+
+    # quantity
+    quantity = ds._generic_check._check_var(
+        quantity, 'quantity',
+        types=str,
+        default='tangency_radius',
+        allowed=['alpha', 'tangency_radius', 'length'],
+    )
+
+    # lim_to_segments
+    lim_to_segments = ds._generic_check._check_var(
+        lim_to_segments, 'lim_to_segments',
+        types=bool,
+        default=False,
+    )
+
+    # segment
+    if segment is not None:
+        segment = np.atleast_1d(segment).astype(int).ravel()
+
+    # --------
+    # tangency radius-specific
+
+    if quantity == 'tangency_radius':
+        # axis_pt
+        if axis_pt is None:
+            axis_pt = [0., 0., 0.]
+
+        axis_pt = ds._generic_check._check_flat1darray(
+            axis_pt, 'axis_pt',
+            dtype=float,
+            size=3,
+            can_be_None=False,
+        )
+
+        # axis_vect
+        if axis_vect is None:
+            axis_vect = [0., 0., 1.]
+
+        axis_vect = ds._generic_check._check_flat1darray(
+            axis_vect, 'axis_vect',
+            dtype=float,
+            size=3,
+            norm=True,
+            can_be_None=False,
+        )
+
+    return key, quantity, segment, lim_to_segments, axis_pt, axis_vect
+
+
+# ###############################################################
+#            tangency radius - prepare data
+# ###############################################################
+
+
+def _tangency_radius_prepare(
+    coll=None,
+    key=None,
+    quantity=None,
+    segment=None,
+    axis_pt=None,
+    axis_vect=None,
+):
+
+    pts_x, pts_y, pts_z = coll.get_rays_pts(key=key)
+    i0 = np.arange(0, pts_x.shape[0])
+
+    # select segment
+    if segment is not None:
+        npts = pts_x.shape[0]
+        segment[segment < 0] = npts - 1 + segment[segment < 0]
+
+        iseg = np.r_[segment, segment[-1] + 1]
+        i0 = i0[iseg]
+        pts_x = pts_x[iseg, :]
+        pts_y = pts_y[iseg, :]
+        pts_z = pts_z[iseg, :]
+
+
+    # define vectors
+    ABx = np.diff(pts_x, axis=0)
+    ABy = np.diff(pts_y, axis=0)
+    ABz = np.diff(pts_z, axis=0)
+
+    if quantity == 'tangency_radius':
+        AOx = axis_pt[0] - pts_x[:-1, ...]
+        AOy = axis_pt[1] - pts_y[:-1, ...]
+        AOz = axis_pt[2] - pts_z[:-1, ...]
+
+        # --------
+        # get kk
+
+        # Eq:
+        # k^2 |AB x v|^2 + 2k((AB.v)(AO.v) - AB.AO) + |AO x v|^2 = dist^2
+        #
+        # minimum:
+        # k |AB x v|^2 + (AB.v)(AO.v) - AB.AO = 0
+
+        ABvn2 = (
+            (ABy*axis_vect[2] - ABz*axis_vect[1])**2
+            + (ABz*axis_vect[0] - ABx*axis_vect[2])**2
+            + (ABx*axis_vect[1] - ABy*axis_vect[0])**2
+            )
+        AOvn2 = (
+            (AOy*axis_vect[2] - AOz*axis_vect[1])**2
+            + (AOz*axis_vect[0] - AOx*axis_vect[2])**2
+            + (AOx*axis_vect[1] - AOy*axis_vect[0])**2
+            )
+
+        ABv = ABx*axis_vect[0] + ABy*axis_vect[1] + ABz*axis_vect[2]
+        AOv = AOx*axis_vect[0] + AOy*axis_vect[1] + AOz*axis_vect[2]
+        ABAO = ABx*AOx + ABy*AOy + ABz*AOz
+        B = ABv*AOv - ABAO
+
+    else:
+        AOx, AOy, AOz = None, None, None
+        ABvn2, AOvn2, B = None, None, None
+        i0 = None
+
+    return (
+        pts_x, pts_y, pts_z,
+        ABx, ABy, ABz,
+        AOx, AOy, AOz,
+        ABvn2, AOvn2, B,
+        i0,
+    )
+
+
+# ###############################################################
+# ###############################################################
+#                  intersect radius
+# ###############################################################
 
 
 def intersect_radius(
