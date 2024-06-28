04a1f576117f08a63f83bd4f13b84cf5340a3e21
Didier Vezinet <dvezinet@cfs.energy>
2024-06-28 18:55:34 -0400

    [#946] Advanced, TBF

--- tofu/data/_class08_save2json.py
+++ tofu/data/_class08_save2json.py
@@ -28,6 +28,9 @@ from . import _class02_save2stp
 _NAME = 'test'
 
 
+
+
+
 # #################################################################
 # #################################################################
 #          Main
@@ -58,6 +61,7 @@ def main(
 
     (
         key, key_cam,
+        lcls,
         factor, color,
         pfe_save, overwrite,
     ) = _check(
@@ -79,17 +83,34 @@ def main(
     # get file content
     # ----------------
 
-    # HEADER
+    # -----------
+    # Header
+
     msg_header = _get_header(
         fname=fname,
     )
 
-    # DATA
-    dout = _extract(
+    # -------------
+    # Diagnostic
+
+    # initialze
+    dout = {}
+
+    # fill
+    dout['diagnostic'] = _extract_diagnostic(
         coll=coll,
         key=key,
         key_cam=key_cam,
     )
+
+    # ------------
+    # list of classes
+
+    for kcls, lkeys in dcls.items():
+        dout[kcls] = _DFUNC[kcls](
+            coll=coll,
+            keys=lkeys,
+        )
 
     # -------------
     # save to stp
@@ -203,110 +224,204 @@ def _get_header(fname=None):
 
 # #################################################################
 # #################################################################
-#          extract
+#          extract - diagnostic
 # #################################################################
 
 
-def _extract(
+def _extract_diagnostic(
     coll=None,
     key=None,
     key_cam=None,
+    excluded=None,
 ):
+
+    # ----------------------
+    # check inputs
+    # ----------------------
+
+    exdef = [
+        'doptics',
+        'signal',
+        'nb geom matrix'
+    ]
+    if isinstance(excluded, str):
+        excluded = [excluded]
+    excluded = ds._generic_check._check_var_iter(
+        excluded, 'excluded',
+        default=exdef,
+        types=list,
+        types_iter=str,
+    )
 
     # ----------------------
     # initialize
     # ----------------------
 
-    dout = {}
+    # initialze with simple values
+    dout = {
+        k0: v0
+        for k0, v0 in coll.dobj['diagnostic'][key].items()
+        if not (
+                isinstance(v0, dict)
+                or k0 in excluded
+            )
+    }
+
+    # prepare doptics extraction
+    dout['doptics'] = {}
+    lcam = coll.dobj['diagnostic'][key]['camera']
+    doptics = coll.dobj['diagnostic'][key]['doptics']
 
     # ----------------------
     # extract points
     # ----------------------
 
-    for i0, k0 in enumerate(key_optics):
+    for i0, kcam in enumerate(lcam):
 
-        # points of polygons
-        ptsx, ptsy, ptsz = coll.get_optics_poly(
-            key=k0,
-            add_points=False,
-            # min_threshold=4e-3,
-            min_threshold=None,
-            mode=None,
-            closed=True,
-            ravel=None,
-            total=True,
-            return_outline=False,
-        )
+        # -----------------------------
+        # initialize with simple values
 
-        # store
-        if ptsx.ndim == 1:
-            dptsx[k0], dptsy[k0], dptsz[k0] = ptsx, ptsy, ptsz
+        dout['doptics'][kcam] = {
 
-        elif ptsx.ndim == 2:
-            for ii in range(ptsx.shape[1]):
-                key = f"{k0}_{ii}"
-                dptsx[key] = ptsx[:, ii]
-                dptsy[key] = ptsy[:, ii]
-                dptsz[key] = ptsz[:, ii]
+        }
 
-        else:
-            raise NotImplementedError(str(ptsx.shape))
 
-        # unit vectors
-        k0, cls = coll.get_optics_cls(optics=k0)
-        k0, cls = k0[0], cls[0]
-        if cls == 'camera':
 
-            if coll.dobj[cls][k0]['dgeom']['parallel'] is True:
-                duvect[k0] = {
-                    'nin': coll.dobj[cls][k0]['dgeom']['nin'],
-                    'e0': coll.dobj[cls][k0]['dgeom']['e0'],
-                    'e1': coll.dobj[cls][k0]['dgeom']['e1'],
-                }
+    return dout
 
-            else:
-                dv = coll.get_camera_unit_vectors(k0)
-                lv = [
-                    'nin_x', 'nin_y', 'nin_z',
-                    'e0_x', 'e0_y', 'e0_z',
-                    'e1_x', 'e1_y', 'e1_z',
-                ]
-                nin_x, nin_y, nin_z, e0x, e0y, e0z, e1x, e1y, e1z = [
-                    dv[k1] for k1 in lv
-                ]
 
-                if e0x.ndim == 1:
-                    for ii in range(ptsx.shape[1]):
-                        key = f"{k0}_{ii}"
-                        duvect[key] = {
-                            'nin': np.r_[nin_x[ii], nin_y[ii], nin_z[ii]],
-                            'e0': np.r_[e0x[ii], e0y[ii], e0z[ii]],
-                            'e1': np.r_[e1x[ii], e1y[ii], e1z[ii]],
-                        }
+# #################################################################
+# #################################################################
+#          extract - camera
+# #################################################################
 
-                else:
-                    raise NotImplementedError(str(e0x.shape))
 
-        else:
-            duvect[k0] = {
-                'nin': coll.dobj[cls][k0]['dgeom']['nin'],
-                'e0': coll.dobj[cls][k0]['dgeom']['e0'],
-                'e1': coll.dobj[cls][k0]['dgeom']['e1'],
-            }
+def _extract_camera(
+    coll=None,
+    keys=None,
+    excluded=None,
+):
 
-    # ------------------------
-    # correspondence pts vect
+    # ----------------------
+    # check inputs
+    # ----------------------
 
-    dcor_ptsvect = {}
-    for k0 in dptsx.keys():
-        if k0 in duvect.keys():
-            dcor_ptsvect[k0] = k0
-        else:
-            key = '_'.join(k0.split('_')[:-1])
-            assert key in duvect.keys(), (key, duvect.keys())
-            dcor_ptsvect[k0] = key
+    exdef = [
+        'doptics',
+        'signal',
+        'nb geom matrix'
+    ]
+    if isinstance(excluded, str):
+        excluded = [excluded]
+    excluded = ds._generic_check._check_var_iter(
+        excluded, 'excluded',
+        default=exdef,
+        types=list,
+        types_iter=str,
+    )
 
-    return dptsx, dptsy, dptsz, duvect, dcor_ptsvect
+    # ----------------------
+    # initialize
+    # ----------------------
+
+    # initialze with simple values
+    dout = {
+        k0: v0
+        for k0, v0 in coll.dobj['camera'][key].items()
+        if not (
+                isinstance(v0, dict)
+                or k0 in excluded
+            )
+    }
+
+    # prepare doptics extraction
+    dout['doptics'] = {}
+    lcam = coll.dobj['diagnostic'][key]['camera']
+    doptics = coll.dobj['diagnostic'][key]['doptics']
+
+    # ----------------------
+    # extract points
+    # ----------------------
+
+    for i0, kcam in enumerate(lcam):
+
+        # -----------------------------
+        # initialize with simple values
+
+        dout['doptics'][kcam] = {
+
+        }
+
+
+
+    return dout
+
+
+# #################################################################
+# #################################################################
+#          extract - aperture
+# #################################################################
+
+
+def _extract_aperture(
+    coll=None,
+    keys=None,
+    excluded=None,
+):
+
+    # ----------------------
+    # check inputs
+    # ----------------------
+
+    exdef = [
+        'doptics',
+        'signal',
+        'nb geom matrix'
+    ]
+    if isinstance(excluded, str):
+        excluded = [excluded]
+    excluded = ds._generic_check._check_var_iter(
+        excluded, 'excluded',
+        default=exdef,
+        types=list,
+        types_iter=str,
+    )
+
+    # ----------------------
+    # initialize
+    # ----------------------
+
+    # initialze with simple values
+    dout = {
+        k0: v0
+        for k0, v0 in coll.dobj['camera'][key].items()
+        if not (
+                isinstance(v0, dict)
+                or k0 in excluded
+            )
+    }
+
+    # prepare doptics extraction
+    dout['doptics'] = {}
+    lcam = coll.dobj['diagnostic'][key]['camera']
+    doptics = coll.dobj['diagnostic'][key]['doptics']
+
+    # ----------------------
+    # extract points
+    # ----------------------
+
+    for i0, kcam in enumerate(lcam):
+
+        # -----------------------------
+        # initialize with simple values
+
+        dout['doptics'][kcam] = {
+
+        }
+
+
+
+    return dout
 
 
 # #################################################################
@@ -346,3 +461,18 @@ def _save(
     print(msg)
 
     return
+
+
+# #################################################################
+# #################################################################
+#          DICT of FUNCTIONS
+# #################################################################
+
+
+_DFUNC = {
+    'camera': _extract_camera,
+    'aperture': _extract_aperture,
+    'filter': _extract_filter,
+    'crystal': _extract_crystal,
+    'grating': _extract_grating,
+}
