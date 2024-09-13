f158a03a67cf135b32ed98027c215368dece7e92
Didier Vezinet <dvezinet@cfs.energy>
2024-09-13 07:02:51 -0400

    [#973] pinned down setuptools version to !=65.*,!=74.* for msvccompiler

--- setup.py
+++ setup.py
@@ -314,7 +314,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        "setuptools==70.0.0",
+        "setuptools!=65.*,!=74.*",
         "numpy",
         "scipy",
         # "scikit-sparse",
