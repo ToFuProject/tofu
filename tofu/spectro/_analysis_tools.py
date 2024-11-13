958c486e1673b3e023a80c8ea14d158ce5d48905
Didier Vezinet <dvezinet@cfs.energy>
2024-11-13 12:46:13 -0500

    [#983] cleaned up spectro/_fit12d_dinput.py

--- tofu/spectro/_analysis_tools.py
+++ tofu/spectro/_analysis_tools.py
@@ -17,10 +17,16 @@ __all__ = [
 ]
 
 
-_LTYPES = (int, float, np.integer, np.float64)
-
 _GITHUB = 'https://github.com/ToFuProject/tofu/issues'
 _WINTIT = f'tofu-{__version__}\treport issues / requests at {_GITHUB}'
+
+
+# Useful scalar types
+_NINT = (np.int32, np.int64)
+_INT = (int,) + _NINT
+_NFLOAT = (np.float32, np.float64)
+_FLOAT = (float,) + _NFLOAT
+_NUMB = _INT + _FLOAT
 
 
 ###########################################################
@@ -127,7 +133,7 @@ def _get_localextrema_1d_check(
             width = 0.
         else:
             width = False
-    c0 = width is False or (isinstance(width, _LTYPES) and width >= 0.)
+    c0 = width is False or (isinstance(width, _NUMB) and width >= 0.)
     if not c0:
         msg = (
             "Arg width must be a float\n"
@@ -146,7 +152,7 @@ def _get_localextrema_1d_check(
 
     if rel_height is None:
         rel_height = 0.8
-    if not (isinstance(rel_height, _LTYPES) and 0 <= rel_height <= 1.):
+    if not (isinstance(rel_height, _NUMB) and 0 <= rel_height <= 1.):
         msg = (
             "Arg rel_height must be positive float in [0, 1]!\n"
             + "Provided: {}".format(rel_height)
