4f6ad1b7558e37abf1986722a94f9442690d22a9
Didier Vezinet <dvezinet@cfs.energy>
2024-12-20 06:21:47 -0500

    [#1002] color cycle for LOS

--- tofu/data/_class8_plot.py
+++ tofu/data/_class8_plot.py
@@ -620,7 +620,7 @@ def _plot_diagnostic(
                     nan_los,
                     nan_los,
                     nan_los,
-                    c=color_dict['x'][ii],
+                    c=color_dict['x'][ii%len(color_dict['x'])],
                     ls='-',
                     lw=1.,
                 )
@@ -775,7 +775,7 @@ def _plot_diagnostic(
                     l0, = ax.plot(
                         dataz,
                         ddata[k0][sli],
-                        c=color_dict['x'][ii],
+                        c=color_dict['x'][ii%len(color_dict['x'])],
                         lw=1.,
                         ls='-',
                     )
@@ -1185,7 +1185,7 @@ def _add_camera_los_cross(
             l0, = ax.plot(
                 nan_los,
                 nan_los,
-                c=color_dict['x'][ii],
+                c=color_dict['x'][ii%len(color_dict['x'])],
                 ls='-',
                 lw=1.,
             )
@@ -1209,7 +1209,7 @@ def _add_camera_los_cross(
             l0, = ax.fill(
                 nan_vos,
                 nan_vos,
-                fc=color_dict['x'][ii],
+                fc=color_dict['x'][ii%len(color_dict['x'])],
                 alpha=alpha,
                 ls='None',
                 lw=0.,
@@ -1251,7 +1251,7 @@ def _add_camera_los_hor(
             l0, = ax.plot(
                 nan_los,
                 nan_los,
-                c=color_dict['x'][ii],
+                c=color_dict['x'][ii%len(color_dict['x'])],
                 ls='-',
                 lw=1.,
             )
@@ -1277,7 +1277,7 @@ def _add_camera_los_hor(
                 l0, = ax.fill(
                     nan_vos,
                     nan_vos,
-                    fc=color_dict['x'][ii],
+                    fc=color_dict['x'][ii%len(color_dict['x'])],
                     alpha=alpha,
                     ls='None',
                     lw=0.,
@@ -1324,7 +1324,7 @@ def _add_camera_vlines_marker(
                 ddatay[k0][0:1],
                 marker='s',
                 ms=6,
-                markeredgecolor=color_dict['x'][ii],
+                markeredgecolor=color_dict['x'][ii%len(color_dict['x'])],
                 markerfacecolor='None',
             )
 
