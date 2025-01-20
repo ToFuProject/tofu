91f7ae139e579c3b791336c79e85d2eece1982a2
Didier Vezinet <dvezinet@cfs.energy>
2025-01-20 15:52:44 -0500

    [#1014] Progressing on EQDSK

--- tofu/data/_class01_eqdsk.py
+++ tofu/data/_class01_eqdsk.py
@@ -7,15 +7,94 @@ Created on Mon Nov 13 14:41:08 2023
 
 # Built-in
 import os
+import warnings
 
 
 # Common
 import numpy as np
 import datastock as ds
-import bsplines2d as bs2
+
+
+from ._utils_types import *
 
 
 __all_ = ['load_eqdsk']
+
+
+# ########################################################
+# ########################################################
+#               Units
+# ########################################################
+
+
+_D0 = {
+    'nx': None,
+    'ny': None,
+}
+
+
+_DUNITS = {
+    'comment': {
+        'units': None,
+    },
+    'current': {
+        'units': 'A',
+    },
+    'cplasma': {
+        'units': 'A',
+    },
+    'pres': {
+        'units': None,
+    },
+    'pressure': {
+        'units': None,
+    },
+    'psi': {
+        'units': None,
+    },
+    'psi_axis': {
+        'units': None,
+    },
+    'psi_boundary': {
+        'units': None,
+    },
+    'psirz': {
+        'units': None,
+    },
+    # Magnetic axis
+    'rmagx': {
+        'units': 'm',
+    },
+    'zmagx': {
+        'units': 'm',
+    },
+    'rmaxis': {
+        'units': 'm',
+    },
+    'zmaxis': {
+        'units': 'm',
+    },
+    'rcentr': {
+        'units': 'm',
+    },
+    # grad-shafranov
+    'ffprime': {
+        'units': '',
+    },
+    'pprime': {
+        'units': '',
+    },
+    # others
+    'shot': {
+        'units': 'm',
+    },
+    'xlim': {
+        'units': 'm',
+    },
+    'ylim': {
+        'units': 'm',
+    },
+}
 
 
 # ########################################################
@@ -27,7 +106,6 @@ __all_ = ['load_eqdsk']
 def load_eqdsk(
     dpfe=None,
     returnas=None,
-    strict=None,
     # keys
     kmesh=None,
     # optional time
@@ -35,6 +113,11 @@ def load_eqdsk(
     ktime=None,
     knt=None,
     t_units=None,
+    # group naming
+    func_key_groups=None,
+    # optipns
+    verb=None,
+    strict=None,
 ):
     """ load multiple eqdsk equilibria files and concatenate them
 
@@ -70,24 +153,78 @@ def load_eqdsk(
     # check inputs
     # --------------------
 
-    lpfe, returnas, coll, kmesh, t, ktime, knt, t_units, geqdsk = check_inputs(
+    (
+        lpfe, returnas, coll,
+        kmesh, t, ktime, knt, t_units,
+        geqdsk,
+        func_key_groups,
+        verb,
+        strict,
+    ) = check_inputs(
         dpfe=dpfe,
         returnas=returnas,
         kmesh=kmesh,
-        strict=strict,
         # optional time
         t=t,
         ktime=ktime,
         knt=knt,
         t_units=t_units,
+        # group naming
+        func_key_groups=func_key_groups,
+        # options
+        verb=verb,
+        strict=strict,
+    )
+
+    # ----------------
+    # double-check shapes
+    # ----------------
+
+    lpfe, dout, dgroups = _check_shapes(
+        lpfe=lpfe,
+        geqdsk=geqdsk,
+        # group keys
+        func_key_groups=func_key_groups,
+        # options
+        verb=verb,
+        strict=strict,
     )
 
     # ----------------
     # load and extract
     # ----------------
 
+
+    for ig, (kg, vg) in enumerate(dgroups.items()):
+
+        print()
+        print(f"\n--- group {ig} {kg} ----\n")
+
+        dref, ddata = _initialize(
+            dgroups=dgroups,
+            kgroup=kg,
+            latt=vg['lattr'],
+            ltypes=vg['types'],
+            dout=dout[vg['pfe'][0]]
+        )
+
+        for ip, pfe in enumerate(vg['pfe']):
+            print()
+            print(ip, pfe)
+
+            for ia, (attr, typ) in enumerate(zip(vg['lattr'], vg['types'])):
+
+                # check identical
+                if attr in _D0.keys():
+                    if ip == 0:
+                        _fill(kattr, typ, dref, ddata, dout[pfe])
+                    else:
+                        _check(kattr, typ, dref, ddata, dout[pfe])
+                else:
+                    _fill(kattr, typ, dref, ddata, dout[pfe])
+
+
     # loop on all files
-    dfail = {}
     npfe = len(lpfe)
     for ii, pfe in enumerate(lpfe):
 
@@ -200,13 +337,17 @@ def load_eqdsk(
 def check_inputs(
     dpfe=None,
     returnas=None,
-    strict=None,
     # keys
     kmesh=None,
     t=None,
     knt=None,
     ktime=None,
     t_units=None,
+    # group naming
+    func_key_groups=None,
+    # optipns
+    verb=None,
+    strict=None,
 ):
 
     # --------------------
@@ -243,11 +384,11 @@ def check_inputs(
         returnas = True
 
     coll = None
+    from ._class10_Inversion import Inversion as Collection
     if returnas is True:
-        from ._class10_Inversion import Inversion as Collection
         coll = Collection()
 
-    elif issubclass(returnas.__class__, bs2.BSplines2D):
+    elif issubclass(returnas.__class__, Collection):
         coll = returnas
 
     elif returnas is not dict:
@@ -311,7 +452,335 @@ def check_inputs(
     else:
         t, ktime, knt, t_units = None, None, None, None
 
-    return lpfe, returnas, coll, kmesh, t, ktime, knt, t_units, geqdsk
+    # ---------------
+    # group naming
+    # ---------------
+
+    if func_key_groups is None:
+        func_key_groups = lambda pfe, i0: f"eqdsk{i0}"
+
+    else:
+        try:
+            func_key_groups('', 0)
+        except Exception as err:
+            msg = (
+                "If provided, func_key_groups must be a callable of 2 args:\n"
+                "\t- pfe: str to file\n"
+                "\t- ii: int\n"
+                "Provided:\n{func_key_groups}\n"
+            )
+            raise Exception(msg) from err
+
+    # ---------------
+    # verb
+    # ---------------
+
+    verb = ds._generic_check._check_var(
+        verb, 'verb',
+        types=bool,
+        default=True,
+    )
+
+    # ---------------
+    # strict
+    # ---------------
+
+    strict = ds._generic_check._check_var(
+        strict, 'strict',
+        types=bool,
+        default=False,
+    )
+
+    return (
+        lpfe, returnas, coll,
+        kmesh, t, ktime, knt, t_units,
+        geqdsk,
+        func_key_groups,
+        verb,
+        strict,
+    )
+
+
+# ########################################################
+# ########################################################
+#               check shapes
+# ########################################################
+
+
+def _check_shapes(
+    lpfe=None,
+    geqdsk=None,
+    # group keys
+    func_key_groups=None,
+    # options
+    verb=None,
+    strict=None,
+):
+
+    # ------------------
+    # prepare
+    # -----------------
+
+    # loop on all files
+    dfail = {}
+    dgroups = {}
+    dout = {}
+
+    # ------------------
+    # loop on files
+    # -----------------
+
+    i0 = 0
+    for ii, pfe in enumerate(lpfe):
+
+        # --------------
+        # open and load
+
+        try:
+            with open(pfe, "r") as ff:
+                data = geqdsk.read(ff)
+        except Exception as err:
+            if strict is True:
+                raise err
+            dfail[pfe] = err
+            continue
+
+        # -----------------
+        # sorted attributes
+
+        # attributes
+        lattr = tuple(sorted(
+            [k0 for k0 in dir(data) if not k0.startswith('__')]
+        ))
+
+        # values
+        lval = [getattr(data, kk) for kk in lattr]
+
+        # types
+        ltypes = [
+            vv.shape if isinstance(vv, np.ndarray)
+            else (
+                'str' if isinstance(vv, str)
+                else 'scalar'
+            )
+            for vv in lval
+        ]
+
+        # unique field_type key
+        field_types = "_".join([
+            f"({kk}, {tt})" for kk, tt in zip(lattr, ltypes)
+        ])
+
+        # -----------
+        # groups
+
+        if field_types not in dgroups.keys():
+            kgroup = func_key_groups(pfe, i0)
+            dgroups[field_types] = {
+                'key': kgroup,
+                'lattr': lattr,
+                'ltypes': ltypes,
+                'pfe': [pfe],
+            }
+            i0 += 1
+
+        else:
+            dgroups[field_types]['pfe'].append(pfe)
+
+        # ----------
+        # initialize
+
+        dout[pfe] = {
+            'group': kgroup,
+            **dout,
+        }
+
+    # ----------------
+    # dfail
+    # ----------------
+
+    if len(dfail) > 0:
+
+        lfail = sorted(dfail.keys())
+        lpath = [os.path.split(pfe) for pfe in lfail]
+        pathu = sorted(set([pp[0] for pp in lpath]))
+        dpathu = {
+            path: [pp[1] for pp in lpath if pp[0] == path]
+            for path in pathu
+        }
+
+        lstr = [
+            f"path = {path}:\n"
+            + "\n".join([
+                f"\t- {pp}: {str(dfail[lfail[os.path.join(path, pp)]])}"
+                for pp in dpathu[path]
+            ])
+            for path in pathu
+        ]
+
+        msg = (
+            "The following files could not be loaded:\n"
+            + "\n".join(lstr)
+        )
+        warnings.warn(msg)
+
+    # -------------
+    # verb
+    # -------------
+
+    if verb is True:
+
+        lstr = [
+            f"\nEQDSK group '{kg}':\n"
+            + "\n".join([
+                f"\t- {katt}: {typ}"
+                for katt, typ in zip(vg['lattr'], vg['ltypes'])
+            ])
+            for kg, vg in dgroups.items()
+        ]
+        msg = (
+            "\n-----------------------\nDetected content of files:\n"
+            + "\n".join(lstr)
+        )
+        print(msg)
+
+    # -------------
+    # adjust lpfe
+    # -------------
+
+    lpfe = [pfe for pfe in lpfe if pfe not in dfail.keys()]
+
+    return lpfe, dout, dgroups
+
+
+# ########################################################
+# ########################################################
+#               Initialize
+# ########################################################
+
+
+def _initialize(
+    dgroups=None,
+    kgroup=None,
+    latt=None,
+    ltypes=None,
+    dout=None,
+):
+
+    # ------------
+    # nb of pfe
+    # ------------
+
+    npfe = len(dgroups[kgroup]['pfe'])
+
+    krpfe = "neq"
+    dref = {
+        krpfe: {
+            'key': krpfe,
+            'size': npfe,
+        },
+    }
+
+    kipfe = 'ieq'
+    ddata= {
+        kipfe: {
+            'key': kipfe,
+            'data': np.arange(0, npfe),
+            'ref': (krpfe,),
+            'dim': 'index',
+            'units': None,
+        },
+    }
+
+    # ------------
+    # R, Z grid
+    # ------------
+
+    R, Z = _extract_grid()
+
+    dmesh = {
+        'mRZ': {
+            'key': km,
+            'knots0': R,
+            'knots1': Z,
+            'units': ('m', 'm'),
+            'deg': 1,
+        },
+    }
+
+    # ------------
+    #
+    # ------------
+
+    for ii, (katt, typ) in enumerate(zip(latt, ltypes)):
+
+        # -----------
+        # ref
+
+
+
+
+        # -----------
+        # data
+
+        if kattr in _D0.keys():
+            ddata[katt] = {
+                'key': katt,
+                'data': dout[katt],
+                'units': _DUNIT.get(kattr),
+                'ref': None,
+            }
+
+        else:
+            if typ == 'str':
+                init = []
+            elif typ == 'scalar':
+                init = np.full((len(dout),), np.nan)
+            else:
+                shape = (len(dout),) + typ
+                init = np.full((len(dout),), np.nan)
+
+            ddata[katt] = {
+                'key': katt,
+                'data': init,
+                'units': _DUNIT.get(kattr),
+                'ref': None,
+            }
+
+
+
+
+    return dref, ddata
+
+
+# ########################################################
+# ########################################################
+#               fill
+# ########################################################
+
+
+def _fill(kattr, typ, ddata, dout, pfe):
+
+    if typ in ('str', 'scalar'):
+        ddata[katt]['data'][ip] = dout[pfe][katt]
+
+    else:
+        sli = None
+        ddata[katt]['data'][sli] = dout[pfe][katt]
+
+    return
+
+
+def _check(kattr, typ, ddata, dout, pfe):
+
+    if typ in ('str', 'scalar'):
+        ddata[katt]['data'][ip] = dout[pfe][katt]
+
+    else:
+        sli = None
+        ddata[katt]['data'][sli] = dout[pfe][katt]
+
+    return
 
 
 # ########################################################
