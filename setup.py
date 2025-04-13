3309f8ba205d93c6f8098b80f3f1199cfd55d51c
dvezinet <didier.vezinet@gmail.com>
2025-04-13 16:20:41 +0200

    [#devel] debug MANIFEST.in for transmission data

--- setup.py
+++ setup.py
@@ -274,8 +274,6 @@ setup(
         # Indicate who your project is intended for
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Physics",
-        # Pick your license as you wish (should match "license" above)
-        "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         "Programming Language :: Python :: 3.6",
