724ead25c37ff531e84f1f9910eaa7686b3f7743
dvezinet <dvezinet@cfs.energy>
2025-09-15 21:23:44 +0000

    [#1132] Restructured to electrons_thermal and electrons_runaways

--- tofu/physics_tools/__init__.py
+++ tofu/physics_tools/__init__.py
@@ -1,6 +1,7 @@
 
 from . import geometry
 from ._distributions import get_maxwellian
-from . import runaways
+from . import electrons_runaways
+from . import electrons_thermal
 from .transmission import get_xray_transmission
 from .heat_transport_1d import main as heat_transport_1d
